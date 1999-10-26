28b87f294fbbb7146d596c6a78d8dfec3ff4e4bc
kuriyama <kuriyama@FreeBSD.org>
1999-10-26 02:37:16 +0000

    19991008 version of XT. Sort PLIST. Change SAX driver in xt-xml4j sample script.  (I don't know this is correct.)
    XSLT becomes "Proposed Recommendation".  Yay!

--- textproc/xt/files/xt-xml4j.sh
+++ textproc/xt/files/xt-xml4j.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-# $FreeBSD$
+# $FreeBSD: ports/textproc/xt/files/xt-xml4j.sh,v 1.2 1999/08/31 02:09:56 peter Exp $
 
 export CLASSPATH=%%PREFIX%%/share/java/classes/xt.jar:%%PREFIX%%/share/java/classes/xml4j.jar:${CLASSPATH}
 
-java -Dcom.jclark.xsl.sax.parser=com.ibm.xml.parser.SAXDriver com.jclark.xsl.sax.Driver $*
+java -Dcom.jclark.xsl.sax.parser=com.ibm.xml.parsers.SAXParser com.jclark.xsl.sax.Driver $*
 
