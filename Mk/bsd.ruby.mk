1c65301d67df4059ee00bb56e4bca07fa1bb050a
knu <knu@FreeBSD.org>
2001-02-19 16:17:17 +0000

    Update to 2001.02.19b, the second snapshot of today's.  The Rubicon test suite found a bug in the previous snapshot.  The 1.6.3 release is drawing near.

--- Mk/bsd.ruby.mk
+++ Mk/bsd.ruby.mk
@@ -3,7 +3,7 @@
 #
 # Created by: Akinori MUSHA <knu@FreeBSD.org>
 #
-# $FreeBSD: ports/Mk/bsd.ruby.mk,v 1.14 2001/02/05 21:34:38 knu Exp $
+# $FreeBSD: ports/Mk/bsd.ruby.mk,v 1.15 2001/02/19 07:54:08 knu Exp $
 #
 
 .if !defined(Ruby_Include)
@@ -95,7 +95,7 @@ RUBY_VERSION?=		1.6.2
 RUBY_SUFFIX?=		# empty
 .endif
 
-RUBY_SNAPSHOTDATE=	2001.02.19
+RUBY_SNAPSHOTDATE=	2001.02.19b
 
 RUBY_PORTVERSION=	${RUBY_VERSION}.${RUBY_SNAPSHOTDATE}
 RUBY_DISTNAME?=		ruby-${RUBY_VERSION}
