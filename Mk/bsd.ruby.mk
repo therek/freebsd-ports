a2e1c0351b1da829f8e44facb4354527c02599c2
knu <knu@FreeBSD.org>
2001-02-19 07:54:08 +0000

    Update to the stable branch's snapshot as of today (2001.02.19), which is meant to be a candidate for the 1.6.3 release.

--- Mk/bsd.ruby.mk
+++ Mk/bsd.ruby.mk
@@ -3,7 +3,7 @@
 #
 # Created by: Akinori MUSHA <knu@FreeBSD.org>
 #
-# $FreeBSD: ports/Mk/bsd.ruby.mk,v 1.13 2001/01/18 19:53:37 knu Exp $
+# $FreeBSD: ports/Mk/bsd.ruby.mk,v 1.14 2001/02/05 21:34:38 knu Exp $
 #
 
 .if !defined(Ruby_Include)
@@ -95,7 +95,7 @@ RUBY_VERSION?=		1.6.2
 RUBY_SUFFIX?=		# empty
 .endif
 
-RUBY_SNAPSHOTDATE=	2001.02.05
+RUBY_SNAPSHOTDATE=	2001.02.19
 
 RUBY_PORTVERSION=	${RUBY_VERSION}.${RUBY_SNAPSHOTDATE}
 RUBY_DISTNAME?=		ruby-${RUBY_VERSION}
