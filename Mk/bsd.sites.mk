5fc479149db80b9cbe72281867ce80ff7d329a41
trevor <trevor@FreeBSD.org>
2001-04-10 03:17:06 +0000

    Follow changed layout at uiarchive.uiuc.edu in MASTER_SITE_CPAN.

--- Mk/bsd.sites.mk
+++ Mk/bsd.sites.mk
@@ -10,7 +10,7 @@
 # be punished accordingly.  DO NOT COMMIT SOMETHING YOU HAVEN'T TESTED.
 # You have been warned. :)
 #
-# $FreeBSD: ports/Mk/bsd.sites.mk,v 1.42 2001/03/30 17:15:11 ben Exp $
+# $FreeBSD: ports/Mk/bsd.sites.mk,v 1.43 2001/04/05 20:47:00 sf Exp $
 #
 
 # Where to put distfiles that don't have any other master site
@@ -142,7 +142,7 @@ MASTER_SITE_PERL_CPAN+=	\
 	ftp://mirror.hiwaay.net/CPAN/modules/by-module/%SUBDIR%/ \
 	ftp://ftp.isu.net.sa/pub/CPAN/modules/by-module/%SUBDIR%/ \
 	ftp://ftp.bora.net/pub/CPAN/modules/by-module/%SUBDIR%/ \
-	ftp://uiarchive.uiuc.edu/pub/lang/perl/CPAN/modules/by-module/%SUBDIR%/ \
+	ftp://uiarchive.uiuc.edu/pub/ftp/ftp.cpan.org/pub/CPAN/modules/by-module/%SUBDIR%/ \
 	ftp://ftp.ucr.ac.cr/pub/Unix/CPAN/modules/by-module/%SUBDIR%/ \
 	http://www.cpan.dk/CPAN/modules/by-module/%SUBDIR%/ \
 	ftp://ftp.cs.colorado.edu/pub/perl/CPAN/modules/by-module/%SUBDIR%/ \
