8a988dc9378ce7e719d6f281d4c1f4b7d5fc95aa
sobomax <sobomax@FreeBSD.org>
2001-04-23 10:39:43 +0000

    Update to 3.1.0.

--- editors/jedit/files/jedit.sh
+++ editors/jedit/files/jedit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $FreeBSD$
+# $FreeBSD: ports/editors/jedit/files/jedit.sh,v 1.3 2001/04/05 10:31:58 sobomax Exp $
 CLASSPATH=${CLASSPATH}:%%PREFIX%%/share/java/jedit/swingall.jar:%%PREFIX%%/share/java/jedit/jedit.jar
 export CLASSPATH
 
