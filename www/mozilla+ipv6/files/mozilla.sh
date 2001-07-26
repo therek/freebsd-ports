35afea094acf485a504c822f951cb44a221be7ef
sumikawa <sumikawa@FreeBSD.org>
2001-07-26 09:56:55 +0000

    Sync with www/mozilla.

--- www/mozilla+ipv6/files/mozilla.sh
+++ www/mozilla+ipv6/files/mozilla.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $FreeBSD$
+# $FreeBSD: ports/www/mozilla+ipv6/files/mozilla.sh,v 1.2 2000/08/03 14:39:06 sobomax Exp $
 
 cd @PREFIX@/lib/mozilla
-exec ./mozilla $*
+exec ./mozilla "$@"
