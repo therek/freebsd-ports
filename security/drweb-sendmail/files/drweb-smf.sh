7967a113005c8ff39927e4f1ac250ead26d9187a
ache <ache@FreeBSD.org>
2001-10-03 07:34:32 +0000

    Configuration tunings

--- security/drweb-sendmail/files/drweb-smf.sh
+++ security/drweb-sendmail/files/drweb-smf.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $FreeBSD: ports/security/drweb-sendmail/files/drweb-smf.sh,v 1.5 2001/08/25 14:14:27 ache Exp $
+# $FreeBSD$
 
 drwebfilter='!!PREFIX!!/sbin/drweb-smf'
 
@@ -8,7 +8,7 @@ case "$1" in
 start)
 	if [ -x $drwebfilter ] ; then
 	    echo -n ' drweb-smf'
-	    $drwebfilter -u/var/run/drwebd -m/var/run/drweb-smf.sock \
+	    $drwebfilter -u/var/run/drwebd.sock -m/var/run/drweb-smf.sock \
 		-a!!PREFIX!!/drweb/infected.!!! -f/tmp -r -x -h -l \
 		-t 160000 -bt -yt -ka -jr -zd \
 		-gpostmaster -epostmaster@!!HOSTNAME!!
