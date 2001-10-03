bcfe36ca4ed664515954c77580b132c59f833c77
ache <ache@FreeBSD.org>
2001-10-03 04:56:34 +0000

    Upgrade to 4.26 (working, but not cleanly configured)

--- security/drweb-sendmail/files/drweb-smf.sh
+++ security/drweb-sendmail/files/drweb-smf.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $FreeBSD: ports/security/drweb-sendmail/files/drweb-smf.sh,v 1.4 2001/08/25 12:31:39 ache Exp $
+# $FreeBSD: ports/security/drweb-sendmail/files/drweb-smf.sh,v 1.5 2001/08/25 14:14:27 ache Exp $
 
 drwebfilter='!!PREFIX!!/sbin/drweb-smf'
 
@@ -9,8 +9,9 @@ start)
 	if [ -x $drwebfilter ] ; then
 	    echo -n ' drweb-smf'
 	    $drwebfilter -u/var/run/drwebd -m/var/run/drweb-smf.sock \
-		-a!!PREFIX!!/drweb/infected.!!! -f/tmp -r -x -h -l -ba \
-		-gpostmaster -epostmaster@your.domain.name
+		-a!!PREFIX!!/drweb/infected.!!! -f/tmp -r -x -h -l \
+		-t 160000 -bt -yt -ka -jr -zd \
+		-gpostmaster -epostmaster@!!HOSTNAME!!
 	fi
 	;;
 stop)
