2b7f3431af9db12bb8c4803ffd62d8b20df4f7d2
sobomax <sobomax@FreeBSD.org>
2001-12-26 11:14:10 +0000

    (forced) Just to clarify: previous change was not submitted by maintainer (as I erroneously stated in the commit message), but by corecode@corecode.ath.cx.

--- devel/libvanessa_logger/files/patch-libvanessa_logger::vanessa_logger.c
+++ devel/libvanessa_logger/files/patch-libvanessa_logger::vanessa_logger.c
@@ -1,5 +1,5 @@
 
-$FreeBSD$
+$FreeBSD: ports/devel/libvanessa_logger/files/patch-libvanessa_logger::vanessa_logger.c,v 1.1 2001/12/26 11:10:58 sobomax Exp $
 
 --- libvanessa_logger/vanessa_logger.c	2001/12/26 11:04:11	1.1
 +++ libvanessa_logger/vanessa_logger.c	2001/12/26 11:06:21
