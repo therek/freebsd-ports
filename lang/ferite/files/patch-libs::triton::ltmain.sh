cfa0639b4b522f3a667de10de6541fe7aa3e7532
sf <sf@FreeBSD.org>
2002-01-19 05:02:32 +0000

    o update to 0.99.4. o change maintainer's e-mail address.
    PR:		33748
    Submitted by:	maintainer

--- lang/ferite/files/patch-libs::triton::ltmain.sh
+++ lang/ferite/files/patch-libs::triton::ltmain.sh
@@ -1,8 +1,8 @@
 
-$FreeBSD: ports/lang/ferite/files/patch-ltmain.sh,v 1.3 2001/11/24 02:43:25 dwcjr Exp $
+$FreeBSD$
 
---- ltmain.sh.orig	Wed Jan  9 08:46:56 2002
-+++ ltmain.sh	Wed Jan  9 12:09:50 2002
+--- libs/triton/ltmain.sh.orig	Wed Jan  9 08:46:56 2002
++++ libs/triton/ltmain.sh	Wed Jan  9 12:09:50 2002
 @@ -958,6 +958,7 @@
  	;;
  
