e62eee00474a39724df78b1ba35e5faa020e599d
tg <tg@FreeBSD.org>
2002-01-22 12:53:28 +0000

    Upgrade python21 to 2.1.2, the latest bugfix release.
    PR:		34078
    Submitted by:	Hye-Shik Chang <perky@fallin.lv>
    
    Update checksum of dependent ports.

--- Mk/bsd.python.mk
+++ Mk/bsd.python.mk
@@ -1,7 +1,7 @@
 # -*- mode: Makefile; tab-width: 4; -*-
 # ex: ts=4
 #
-# $FreeBSD: ports/Mk/bsd.python.mk,v 1.17 2002/01/16 12:23:24 tg Exp $
+# $FreeBSD: ports/Mk/bsd.python.mk,v 1.18 2002/01/16 12:24:32 tg Exp $
 #
 
 .if !defined(Python_Include)
@@ -100,11 +100,11 @@ PYTHON_WRKSRC=		${WRKDIR}/Python-${_PYTHON_PORTVERSION}
 PYDISTUTILS=	${PYTHON_LIBDIR}/distutils/core.py:${PYTHON_PORTSDIR}
 PYXML=			${PYTHON_SITELIBDIR}/_xmlplus/__init__.py:${PORTSDIR}/textproc/py-xml
 
-PYTHON_DISTFILE=	Python-2.1.1.tgz
+PYTHON_DISTFILE=	Python-2.1.2.tgz
 PYTHON_PORTSDIR=	${PORTSDIR}/lang/python21
-PYTHON_REL=			211
+PYTHON_REL=			212
 PYTHON_SUFFIX=		21
-PYTHON_WRKSRC=		${WRKDIR}/Python-2.1.1
+PYTHON_WRKSRC=		${WRKDIR}/Python-2.1.2
 
 # Python-2.0
 .elif ${PYTHON_VERSION} == "python2.0"
