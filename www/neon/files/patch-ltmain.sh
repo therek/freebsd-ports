88eba1b501fd27af93dc04ebd0a8155c0883d76a
olgeni <olgeni@FreeBSD.org>
2002-03-10 20:57:43 +0000

    Install libneon.la, which is needed by the devel/subversion port.
    Submitted by:	Garrett Rooney <rooneg@electricjellyfish.net>

--- www/neon/files/patch-ltmain.sh
+++ www/neon/files/patch-ltmain.sh
@@ -1,5 +1,5 @@
 
-$FreeBSD$
+$FreeBSD: ports/www/neon/files/patch-ltmain.sh,v 1.1 2002/03/02 19:46:12 olgeni Exp $
 
 --- ltmain.sh.orig	Wed Jan 23 11:38:40 2002
 +++ ltmain.sh	Sun Feb  3 01:44:17 2002
@@ -30,16 +30,3 @@ $FreeBSD$
  	  *)
  	    # Add libc to deplibs on all other systems if necessary.
  	    if test "$build_libtool_need_lc" = "yes"; then
-@@ -4179,10 +4190,12 @@
- 	fi
- 
- 	# Install the pseudo-library for information purposes.
-+	if /usr/bin/false; then
- 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
- 	instname="$dir/$name"i
- 	$show "$install_prog $instname $destdir/$name"
- 	$run eval "$install_prog $instname $destdir/$name" || exit $?
-+	fi
- 
- 	# Maybe install the static library, too.
- 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
