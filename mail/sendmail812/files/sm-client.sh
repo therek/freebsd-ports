d0aee30a3e21ae7a851856d90e94e10dcb2ca970
dinoex <dinoex@FreeBSD.org>
2002-04-06 08:17:34 +0000

    - Update to new release: sendmail-8.12.3 - Patches removed that are now in the distribution - More MILTER dokumentation - sm-client.sh.sample have now 'restart' command. - Add standalone startupscript for all sendmail deamons - Update pkg-message
    This version fixes a long-standing MIME (7 to 8-bit) conversion bug
    and several smaller problems, e.g., a possible communication problem
    between the MTA and libmilter, a bug in handling (invalid) addresses
    containing 8-bit characters, a possible problem with small timeouts
    being lost on slow machines if itimers are used, and the handling of
    the 421 reply code and timeouts in the SMTP delivery code.  There are
    two changes in the msp feature that may change the behavior: MX lookups
    are turned on for the SMTP mailers and hence `[localhost]' is used as
    default instead of `localhost', and confTIME_ZONE is set to USE_TZ in
    submit.mc.  A complete list of changes can be found in the release notes.

--- mail/sendmail812/files/sm-client.sh
+++ mail/sendmail812/files/sm-client.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $FreeBSD: ports/mail/sendmail/files/sm-client.sh,v 1.1 2001/10/07 06:40:42 dinoex Exp $
+# $FreeBSD: ports/mail/sendmail/files/sm-client.sh,v 1.2 2002/03/17 18:00:07 dinoex Exp $
 # 
 if ! test -x %%PREFIX%%/sbin/sendmail
 then
@@ -12,11 +12,20 @@ start)
 	echo -n ' sm-msp-queue'
 	;;
 stop)
-	kill `head -1 /var/spool/clientmqueue/sm-client.pid`
-	rm -f /var/spool/clientmqueue/sm-client.pid
+	if test -e /var/spool/clientmqueue/sm-client.pid
+	then
+		kill `head -1 /var/spool/clientmqueue/sm-client.pid`
+		rm -f /var/spool/clientmqueue/sm-client.pid
+	fi
+	;;
+restart)
+	if test -e /var/spool/clientmqueue/sm-client.pid
+	then
+		kill -1 `head -1 /var/spool/clientmqueue/sm-client.pid`
+	fi
 	;;
 *)
-	echo "Usage: ${0##*/}: { start | stop }" 2>&1
+	echo "Usage: ${0##*/}: { start | stop | restart }" 2>&1
 	exit 65
 	;;
 esac
