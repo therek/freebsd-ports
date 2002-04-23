75702c0edf785c5f8ecc2ecb1f3a053db6a43943
sobomax <sobomax@FreeBSD.org>
2002-04-23 12:37:18 +0000

    Update to 1.0.
    PR:		36987
    Submitted by:	king <king@v2project.com>

--- games/robocode/files/robocode.sh
+++ games/robocode/files/robocode.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-# $FreeBSD$
-CLASSPATH=${CLASSPATH}:robocode.jar:resources.jar:tools.jar:.
-export CLASSPATH
+# $FreeBSD: ports/games/robocode/files/robocode.sh,v 1.2 2001/11/08 16:45:19 sobomax Exp $
 
 cd %%PREFIX%%/share/java/robocode
-exec %%JAVAVM%% robocode.Robocode "$@"
+exec %%JAVAVM%% -jar robocode.jar "$@"
