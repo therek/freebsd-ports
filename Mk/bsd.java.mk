9db34fdd66c21f385a8620b4ecff1c610550bedf
znerd <znerd@FreeBSD.org>
2002-04-26 23:23:02 +0000

    Update to IBM JDK 1.3.1 (cxia32131-20020302).
    PR:		37397
    Submitted by:	Hye-Shik Chang <perky@akaraka.yonsei.ac.kr>

--- Mk/bsd.java.mk
+++ Mk/bsd.java.mk
@@ -9,7 +9,7 @@
 # Please send all suggested changes to the maintainer instead of committing
 # them to CVS yourself.
 #
-# $FreeBSD: ports/Mk/bsd.java.mk,v 1.5 2002/04/04 23:01:24 znerd Exp $
+# $FreeBSD: ports/Mk/bsd.java.mk,v 1.6 2002/04/26 22:30:47 znerd Exp $
 #
 
 .if !defined(Java_Include)
@@ -43,7 +43,7 @@ _JAVA_HOME_FREEBSD_1_1=			${LOCALBASE}/jdk1.1.8
 _JAVA_HOME_FREEBSD_1_2=			${LOCALBASE}/jdk1.2.2
 _JAVA_HOME_FREEBSD_1_3=			${LOCALBASE}/jdk1.3.1
 _JAVA_HOME_BLACKDOWN_LINUX_1_2=	${LOCALBASE}/linux-jdk1.2.2
-_JAVA_HOME_IBM_LINUX_1_3=		${LOCALBASE}/linux-ibm-jdk1.3.0
+_JAVA_HOME_IBM_LINUX_1_3=		${LOCALBASE}/linux-ibm-jdk1.3.1
 _JAVA_HOME_SUN_LINUX_1_3=		${LOCALBASE}/linux-jdk1.3.1
 _JAVA_HOME_SUN_LINUX_1_4=		${LOCALBASE}/linux-jdk1.4.0
 
