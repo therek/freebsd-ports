c67339661d79069dfe100edc82c0f3ac5aadb063
pat <pat@FreeBSD.org>
2002-05-04 17:27:55 +0000

    Update to 0.50.35-1
    PR:		37753
    Submitted by:	maintainer

--- devel/newt/files/setup.py
+++ devel/newt/files/setup.py
@@ -1,6 +1,9 @@
-# $FreeBSD$
+# $FreeBSD: ports/devel/newt/files/setup.py,v 1.1 2002/03/17 12:09:03 ijliao Exp $
 import os
 from distutils.core import setup, Extension
+
+LOCALBASE = os.environ['LOCALBASE']
+PREFIX = os.environ['PREFIX']
 
 setup ( name = 'newt',
 	version = '0.50.33',
@@ -9,7 +12,7 @@ setup ( name = 'newt',
 	ext_modules = [ Extension(
        		name='_snack',
 		sources=['snackmodule.c'],
-		include_dirs=['.', os.environ['PREFIX']+'/include'],
-		library_dirs=['.', os.environ['PREFIX']+'/lib'],
+		include_dirs=['.', LOCALBASE+'/include', PREFIX+'/include'],
+		library_dirs=['.', LOCALBASE+'/lib', PREFIX+'/lib'],
 		libraries=['newt', 'popt', 'slang', 'ncurses']
 	)])
