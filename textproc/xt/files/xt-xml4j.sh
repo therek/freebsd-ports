daf7a58790b685ccca4e0daef80ececee276d30e
kuriyama <kuriyama@FreeBSD.org>
2002-07-29 04:34:38 +0000

    Fit to new bsd.java.mk.
    Submitted by:	KUNISHIMA Takeo <kunishi@acm.org>
    PR:		ports/40999

--- textproc/xt/files/xt-xml4j.sh
+++ textproc/xt/files/xt-xml4j.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# $FreeBSD: ports/textproc/xt/files/xt-xml4j.sh,v 1.3 1999/10/26 02:37:16 kuriyama Exp $
+# $FreeBSD: ports/textproc/xt/files/xt-xml4j.sh,v 1.4 2000/07/25 07:23:57 kuriyama Exp $
 
 export CLASSPATH=%%PREFIX%%/share/java/classes/xt.jar:%%PREFIX%%/share/java/classes/xerces.jar:${CLASSPATH}
 
-java -Dcom.jclark.xsl.sax.parser=org.apache.xerces.parsers.SAXParser com.jclark.xsl.sax.Driver $*
+javavm -Dcom.jclark.xsl.sax.parser=org.apache.xerces.parsers.SAXParser com.jclark.xsl.sax.Driver $*
--- textproc/xt/files/xt-xp.sh
+++ textproc/xt/files/xt-xp.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# $FreeBSD$
+# $FreeBSD: ports/textproc/xt/files/xt-xp.sh,v 1.1 2000/07/25 07:23:57 kuriyama Exp $
 
 export CLASSPATH=%%PREFIX%%/share/java/classes/xt.jar:%%PREFIX%%/share/java/classes/xp-0.5.jar:%%PREFIX%%/share/java/classes/sax.jar:${CLASSPATH}
 
-java com.jclark.xsl.sax.Driver $*
+javavm com.jclark.xsl.sax.Driver $*
