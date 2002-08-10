d6f9f70d09e5c350483b799630ad77ee699c1669
znerd <znerd@FreeBSD.org>
2002-08-10 10:31:09 +0000

    Changed the installation directory of the Sun JDK 1.3.x for Linux to linux-sun-jdk1.3.1.
    PR:		41424

--- Mk/bsd.java.mk
+++ Mk/bsd.java.mk
@@ -9,7 +9,7 @@
 # Please send all suggested changes to the maintainer instead of committing
 # them to CVS yourself.
 #
-# $FreeBSD: ports/Mk/bsd.java.mk,v 1.17 2002/08/07 20:36:04 znerd Exp $
+# $FreeBSD: ports/Mk/bsd.java.mk,v 1.18 2002/08/10 09:03:47 znerd Exp $
 #
 
 .if !defined(Java_Include)
@@ -47,7 +47,7 @@ _JAVA_HOME_BLACKDOWN_LINUX_1_2=	${LOCALBASE}/linux-blackdown-jdk1.2.2
 _JAVA_HOME_SUN_LINUX_1_2=		${LOCALBASE}/linux-sun-jdk1.2.2
 _JAVA_HOME_BLACKDOWN_LINUX_1_3=	${LOCALBASE}/linux-blackdown-jdk1.3.1
 _JAVA_HOME_IBM_LINUX_1_3=		${LOCALBASE}/linux-ibm-jdk1.3.1
-_JAVA_HOME_SUN_LINUX_1_3=		${LOCALBASE}/linux-jdk1.3.1
+_JAVA_HOME_SUN_LINUX_1_3=		${LOCALBASE}/linux-sun-jdk1.3.1
 _JAVA_HOME_SUN_LINUX_1_4=		${LOCALBASE}/linux-jdk1.4.0.01
 
 # Set the JDK ports for all recognized JDK's
