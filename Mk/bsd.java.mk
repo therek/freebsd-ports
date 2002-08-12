894e806ff1d00353914ca6d64d26f96402a47d91
znerd <znerd@FreeBSD.org>
2002-08-12 11:02:58 +0000

    Added Blackdown JDK 1.4.1 for Linux.
    See also:	http://www.blackdown.org/java-linux/java2-status/jdk1.4-status.html

--- Mk/bsd.java.mk
+++ Mk/bsd.java.mk
@@ -9,7 +9,7 @@
 # Please send all suggested changes to the maintainer instead of committing
 # them to CVS yourself.
 #
-# $FreeBSD: ports/Mk/bsd.java.mk,v 1.21 2002/08/10 10:54:02 znerd Exp $
+# $FreeBSD: ports/Mk/bsd.java.mk,v 1.22 2002/08/12 10:57:35 znerd Exp $
 #
 
 .if !defined(Java_Include)
@@ -45,6 +45,7 @@ _JAVA_HOME_FREEBSD_1_2=			${LOCALBASE}/jdk1.2.2
 _JAVA_HOME_FREEBSD_1_3=			${LOCALBASE}/jdk1.3.1
 _JAVA_HOME_BLACKDOWN_LINUX_1_2=	${LOCALBASE}/linux-blackdown-jdk1.2.2
 _JAVA_HOME_BLACKDOWN_LINUX_1_3=	${LOCALBASE}/linux-blackdown-jdk1.3.1
+_JAVA_HOME_BLACKDOWN_LINUX_1_4=	${LOCALBASE}/linux-blackdown-jdk1.4.1
 _JAVA_HOME_IBM_LINUX_1_3=		${LOCALBASE}/linux-ibm-jdk1.3.1
 _JAVA_HOME_SUN_LINUX_1_2=		${LOCALBASE}/linux-sun-jdk1.2.2
 _JAVA_HOME_SUN_LINUX_1_3=		${LOCALBASE}/linux-sun-jdk1.3.1
