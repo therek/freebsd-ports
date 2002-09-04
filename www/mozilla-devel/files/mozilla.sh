4f3f5f3b539aa5915851c23d01bffe54ecd4dc94
marcus <marcus@FreeBSD.org>
2002-09-04 00:42:51 +0000

    Fix the path to exec mozilla.
    Submitted by:	John Merryweather Cooper <john_m_cooper@yahoo.com>

--- www/mozilla-devel/files/mozilla.sh
+++ www/mozilla-devel/files/mozilla.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $FreeBSD$
+# $FreeBSD: ports/www/mozilla-devel/files/mozilla.sh,v 1.3 2001/07/18 07:44:50 sobomax Exp $
 
-cd @PREFIX@/lib/mozilla
+cd @PREFIX@/lib/mozilla-devel
 exec ./mozilla "$@"
