ee183a853bc77ab58fce5b87cffca5413f6a9d8a
marcus <marcus@FreeBSD.org>
2002-10-22 03:42:24 +0000

    This commit includes:
    * mozilla-*vendor ports (currently for Mozilla 1.0.1)
    * mozilla* ports (currently for Mozilla 1.1)
    * mozilla-*devel ports (currently for Mozilla 1.2b)
    
    Special thanks goes to trevor for auto-generating plist patches, cy for
    pointing out that the Mozilla startup scripts need to be tailored for
    each version of Mozilla, grog for suggesting that some verbage needs to
    be added to explain the Java plugin messages at startup, and John
    Merryweather Cooper for suggesting a common plugin directory. Of course,
    thanks also goes to the user community for suggestions and support.
    
    These ports offer:
    
    * Complete coexistence with each other
    * A universal ${PREFIX}/lib/browser_plugins directory
    * Auto-generated plists for ease of maintenance
    * More accurate pkg-descr's
    * A pkg-message pointing users to java/jdk13 for the Java plugin
    
    PR:	42870 42941
    Reviewed by:	ports gnome

--- www/mozilla-vendor/files/mozilla.sh
+++ www/mozilla-vendor/files/mozilla.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $FreeBSD$
+# $FreeBSD: ports/www/mozilla-vendor/files/mozilla.sh,v 1.4 2002/09/03 20:45:54 marcus Exp $
 
-cd @PREFIX@/lib/mozilla
+cd @PREFIX@/lib/mozilla-vendor
 exec ./mozilla "$@"
