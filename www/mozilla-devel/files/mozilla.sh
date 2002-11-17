83189ee0ff59185a4f8d5d53f6e6025a3cd9770e
marcus <marcus@FreeBSD.org>
2002-11-17 22:09:06 +0000

    * Make this port a master port which can support slaves * Install .pc files when built with WITH_GTK2 * Don't play with Xft stuff in source files unless we're building with with   Xft support enabled.

--- www/mozilla-devel/files/mozilla.sh
+++ www/mozilla-devel/files/mozilla.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $FreeBSD: ports/www/mozilla-devel/files/mozilla.sh,v 1.3 2001/07/18 07:44:50 sobomax Exp $
+# $FreeBSD: ports/www/mozilla-devel/files/mozilla.sh,v 1.4 2002/09/04 00:42:51 marcus Exp $
 
-cd @PREFIX@/lib/mozilla-devel
+cd @PREFIX@/lib/%%MOZILLA%%
 exec ./mozilla "$@"
