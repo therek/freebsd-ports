204302dbc614b944f8971dec529969a5dedc915e
marcus <marcus@FreeBSD.org>
2002-11-24 00:24:04 +0000

    Make the Mozilla startup scripts a little smarter.  Instead of launching a new Mozilla instance each time, check to see if one already exists, and if so, spawn a new browser tab in the current window.
    PR:	45610
    Submitted by:	Nick Jennings

--- www/seamonkey/files/mozilla.sh
+++ www/seamonkey/files/mozilla.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
-#
-# $FreeBSD: ports/www/mozilla-devel/files/mozilla.sh,v 1.4 2002/09/04 00:42:51 marcus Exp $
 
-cd @PREFIX@/lib/%%MOZILLA%%
-exec ./mozilla "$@"
+MOZILLA_DIR="%%PREFIX%%/lib/%%MOZILLA%%"
+MOZILLA_EXEC="mozilla"
+LOCATION='new-tab'
+
+cd $MOZILLA_DIR                                     || exit 1
+
+# catch calls for mozilla mail
+if [ "$1" = "-mail" ]; then
+   REMOTE_COMMAND="xfeDoCommand (openInbox)"
+else
+   REMOTE_COMMAND="openURL($@, $LOCATION)"
+fi
+
+# process found
+./$MOZILLA_EXEC -remote "ping()"                    &&
+./$MOZILLA_EXEC -remote "$REMOTE_COMMAND"           && exit 0
+
+# no existing process
+exec ./$MOZILLA_EXEC "$@"
+
