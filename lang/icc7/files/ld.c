f59fd72f65ab9fbfc0c7dc2ab472cf96d5062efb
netchild <netchild@FreeBSD.org>
2003-01-11 17:21:41 +0000

    Update icc to 7.0.078.
    Note:
    Object files built with version 6.0 of Intel(R) Compilers may not link
    properly with objects built with version 7.0 of Intel(R) Compilers.
    Rebuilding of the object files is suggested.
    
    Changes between the submitted version and the committed version:
     - update to 078 instead of 075
     - treat excessive parameters in function calls as an error instead
       of a warning (removed compatibility with a not named MS product, as
       suggested by Intel)
     - allow to override the INTEL_FLEXLM_LICENSE variable
    
    From the submitter:
    - Unbreak usage of signal.h on -current and RELENG_5_0.
      (committers note: see below)
    - Fix ownership of installed files when built as non-root.
    - Fix the patched versions of the icc- and the icpc-script to work with options
      containing spaces (e.g. '-DFOOBAR="foo bar"').
    - Install 'clicense' and 'csupport' in the docs-dir even if NOPORTDOCS is
      defined, the later is not optional but required by 'icid'.
    - Move the ld-wrapper to a sub-dir and thus out of what is normally set in
      PATH to prevent footshooting (this isn't perfect but simple until someone
      reworks this port to provide FreeBSD-compatibility in another way... or
      Intel releases a native version).
    - Patch the headers to better fit for FreeBSD (hopefully...).
    
    Notes:	- Icc7 is more picky about unknow options than the previous versions,
    	  i.e. the gcc-options normally supplied in CFLAGS unless altered in
    	  /etc/make.conf. Therefore compilation of ports with icc likely fail
    	  if the standard CFLAGS are not unset (e.g. `make CC=icc CFLAGS=""`)
    	  or replaced by options valid for icc.
    	- When compiling multi-threaded C-code make sure to link with icc and
    	  option '-mt' (for ports e.g. via PTHREAD_CFLAGS) otherwise libc_r
    	  won't be linked in correctly.
    
    Submitted by:	marius@alchemy.franken.de
    
    Additional information:
     - rev 1.20 of src/sys/i386/include/signal.h introduced __aligned which
       isn't handled in sys/cdefs.h for non GCC compilers (at least not in
       publically available sources)
     - the FreeBSD stdarg.h is missing a macro for va_copy() in the non GCC case,
       we handle it in the port, but be prepared to get problems when the base
       system gets fixed
     - we don't use the icc-buildin of alignof()
     - Intel provides a float.h which has different floatingpoint values
       (e.g. MAX_FLOAT) than we have in the base system, in the port we use the
       FreeBSD header (Warner knows about the issue... at least he got a mail
       from me and Marius)
     - we replace __wchar_t with __ct_rune_t because icc has a build-in type
       for it, this affects "typedef __ct_rune_t __wchar_t;" in sys/_types.h.
       It isn't known if this may be evil...
     - icc doesn't need the option "-mt" anymore to generate thread safe code,
       but our FreeBSD version still needs it to be able to choose libc_r
       instead of libc
    
    If a src-committer wants to address any of the above mentioned issues he
    should first contact me, I may have already some fixes in my local tree.

--- lang/icc7/files/ld.c
+++ lang/icc7/files/ld.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2002 Marius Strobl
+ * Copyright (c) 2002-2003 Marius Strobl
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -28,7 +28,7 @@
  * Based on a shell-script written by Dan Nelson <dnelson@allantgroup.com>
  * with some modifications by Alexander Leidinger <netchild@FreeBSD.org>.
  *
- * $FreeBSD: ports/lang/icc/files/ld.c,v 1.3 2002/09/22 13:05:51 netchild Exp $
+ * $FreeBSD: ports/lang/icc/files/ld.c,v 1.4 2002/11/08 15:59:38 netchild Exp $
  */
 
 /* Uses code marked: */
@@ -185,6 +185,11 @@ main(int argc, char *argv[], char *envp[])
 			continue;
 		}
 
+	 	if (ARGCMP("-MT")) {
+			threaded++;
+			continue;
+	    	}
+
 	 	if (ARGCMP("-PIC")) {
 			pic++;
 			continue;
@@ -212,19 +217,8 @@ main(int argc, char *argv[], char *envp[])
 		}
 
 		/*
-		 * ICC links the thread safe libircmt instead of libirc when
-		 * told to generate threaded code by any of the compiler flags
-		 * "-mt", "-openmp" or "-parallel". We use this as an indicator
-		 * to link against libc_r.
-		 */
-		if (ARGCMP("-lircmt")) {
-			threaded++;
-			continue;
-		}
-
-		/*
-		 * Link against libc_p when "-pg" was given, "/usr/lib/gcrt1.o"
-		 * indicates this.
+		 * Link against libc_p when "-qp" or "-p" were given,
+		 * "/usr/lib/gcrt1.o" indicates this.
 		 */
 		if (ARGCMP("/usr/lib/gcrt1.o")) {
 			gprof++;
@@ -266,7 +260,7 @@ main(int argc, char *argv[], char *envp[])
 
 	for (i = 0; i < argc; i++) {
 	 	if (ARGCMP("-CPLUSPLUS") || ARGCMP("-BOOTSTRAPSTLPORT") ||
-		    ARGCMP("-PIC"))
+		    ARGCMP("-MT") || ARGCMP("-PIC"))
 			continue;
 
 		/* prepend "-melf_i386" to the commandline */
@@ -279,15 +273,6 @@ main(int argc, char *argv[], char *envp[])
 #endif
 			continue;
 		}
-
-		/*
-		 * "-u ___pseudo_link" triggers linking of additional objects
-		 * from libcxa which seem to bloat the binaries, i.e. they
-		 * perfectly work without it. Intel Support promised to look
-		 * up what this servers for...
-		 */
-	 	if (ARGCMP("-u") || ARGCMP("___pseudo_link"))
-			continue;
 
 		/* Don't add obsolete flag "-Qy". */
 		if (ARGCMP("-Qy"))
@@ -358,16 +343,6 @@ main(int argc, char *argv[], char *envp[])
 			free(temp);
 			continue;
 		}
-
-		/*
-		 * Link and map files for C++ exception handling.
-		 */
-		if (!cpp &&
-		    (ARGCMP("--version-script") ||
-		    ARGCMPB(prefix, "/intel/compiler60/ia32/lib/icrt.link") ||
-		    ARGCMPB(prefix,
-			"/intel/compiler60/ia32/lib/icrt.internal.map")))
-			continue;
 
 		/*
 		 * Force libcxa and libunwind to static linkage, since the
