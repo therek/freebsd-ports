46dc0db8bb24f552732d40100430ddd4ff28fc09
ume <ume@FreeBSD.org>
2003-02-04 15:55:54 +0000

    Update to 2.1.12.

--- mail/cyrus-imapd23/files/imapd.sh
+++ mail/cyrus-imapd23/files/imapd.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 
 # Start or stop cyrus-imapd
-# $FreeBSD$
+# $FreeBSD: ports/mail/cyrus-imapd2/files/imapd.sh,v 1.4 2001/04/12 19:06:31 ume Exp $
 
 case "$1" in
 start)
 	if [ -x /usr/local/cyrus/bin/master -a \
 	     -f /usr/local/etc/cyrus.conf -a \
 	     -f /usr/local/etc/imapd.conf ]; then
-		/usr/local/cyrus/bin/master &
+		/usr/local/cyrus/bin/master -d
 		echo -n ' imapd'
 	fi
 	;;
 stop)
-	kill `ps -U cyrus | awk '/master/ {print $1}'` && echo -n ' imapd'
+	if [ -r /var/run/cyrus-master.pid ]; then
+		kill `cat /var/run/cyrus-master.pid` && echo -n ' imapd'
+		rm /var/run/cyrus-master.pid
+	fi
 	;;
 *)
 	echo "Usage: `basename $0` {start|stop}" >&2
