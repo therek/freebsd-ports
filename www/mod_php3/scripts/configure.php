4cd7b33d26704b26424deb8f3cf02a4c41839e32
dirk <dirk@FreeBSD.org>
2003-02-06 09:13:17 +0000

    Follow mod_php4:
    Help "make describe" to find dependencies properly.

--- www/mod_php3/scripts/configure.php
+++ www/mod_php3/scripts/configure.php
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $FreeBSD: ports/www/mod_php3/scripts/configure.php,v 1.119 2003/01/03 19:24:34 ijliao Exp $
+# $FreeBSD: ports/www/mod_php3/scripts/configure.php,v 1.120 2003/01/04 16:17:27 seanc Exp $
 
 if [ -f ${WRKDIRPREFIX}${REALCURDIR}/Makefile.inc ]; then
 	exit
@@ -92,6 +92,7 @@ while [ "$1" ]; do
 		\"MySQL\")
 			echo "LIB_DEPENDS+=	mysqlclient.10:\${PORTSDIR}/databases/mysql323-client"
 			echo "CONFIGURE_ARGS+=--with-mysql=\${PREFIX}"
+			MYSQL=1
 			;;
 		\"PostgreSQL\")
 			echo "POSTGRESQL_PORT?=	databases/postgresql7"
@@ -179,3 +180,8 @@ done
 if [ "${LIBS}" ]; then
 	echo "CONFIGURE_ENV+=	LIBS='${LIBS}'"
 fi
+
+if [ -z "${MYSQL}" ]; then
+	echo "WITHOUT_MYSQL=	1"
+fi
+
