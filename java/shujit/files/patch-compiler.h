6d2e1d44a01afbc1f406760a08386bfc00de4691
sobomax <sobomax@FreeBSD.org>
2003-02-17 15:20:08 +0000

    Fix on -stable.
    Submitted by:	Palle Girgensohn <girgen@pingpong.net>

--- java/shujit/files/patch-compiler.h
+++ java/shujit/files/patch-compiler.h
@@ -1,5 +1,5 @@
 
-$FreeBSD$
+$FreeBSD: ports/java/shujit/files/patch-compiler.h,v 1.3 2003/02/16 08:33:22 sobomax Exp $
 
 --- compiler.h	2003/02/16 00:04:39	1.1
 +++ compiler.h	2003/02/16 00:05:09
@@ -8,7 +8,7 @@ $FreeBSD$
  // Additional type definition
  
 -#if !(defined(_ILP32) || defined(_LP64) || defined(_SYS_INTTYPES_H_) /* for FreeBSD */)
-+#if !(defined(_ILP32) || defined(_LP64) || !defined(_SYS_INTTYPES_H_) /* for FreeBSD */)
++#if !(defined(_ILP32) || defined(_LP64) || defined(__FreeBSD__))
  #  ifndef _UINT16_T
  #    define _UINT16_T
  typedef unsigned short uint16_t;
