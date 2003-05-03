82df672e2f8236f5735d36fbc57462fedffa9809
netchild <netchild@FreeBSD.org>
2003-05-03 15:56:24 +0000

    Unbreak port by updating to 4.23.
    Triggered by:	kris wants to remove broken ports
    Approved by:	maintainer timeout (he hasn't much time at the moment
    		and I want to have bugs get reported just in time to
    		have it fixed before 5.1)

--- audio/csound/files/patch-aops.c
+++ audio/csound/files/patch-aops.c
@@ -1,4 +1,4 @@
-$FreeBSD$
+$FreeBSD: ports/audio/csound/files/patch-aops.c,v 1.2 2002/07/15 00:11:09 trevor Exp $
 
 --- aops.c.orig	Sun Jul 14 16:03:01 2002
 +++ aops.c	Sun Jul 14 16:02:49 2002
@@ -8,11 +8,11 @@ $FreeBSD$
  #include "aops.h"
  #include <math.h>
 @@ -442,8 +443,6 @@ void ftsr(EVAL *p)              /**** ft
+ }
+ 
  #ifdef LINUX
- void rtclock(EVAL *p)
- {
 -#include <sys/time.h>
 -#include <unistd.h>
+ void rtclock(EVAL *p)
+ {
      struct timeval tv;
-     struct timezone tz;
-     gettimeofday(&tv, &tz);
