8821aee372f194a057fead0c5b5144ac9e23fa1d
nork <nork@FreeBSD.org>
2003-06-25 01:35:17 +0000

    Improve SNMP support (fix openssl dependency and add net-snmp5 support).
    PR:		ports/53687
    Submitted by:	Alex Dupre <sysadmin@alexdupre.com> (maintainer)

--- lang/php5/scripts/configure.php
+++ lang/php5/scripts/configure.php
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $FreeBSD: ports/lang/php4/scripts/configure.php,v 1.3 2003/06/20 05:25:24 nork Exp $
+# $FreeBSD: ports/lang/php4/scripts/configure.php,v 1.4 2003/06/24 08:16:25 nork Exp $
 
 if [ -f ${WRKDIR}/Makefile.inc ]; then
 	exit
@@ -64,7 +64,7 @@ READLINE	"readline support (CLI only)" ${WITH_READLINE:-OFF} \
 RECODE		"recode support" ${WITH_RECODE:-OFF} \
 SESSION		"session support" ${WITH_SESSION:-OFF} \
 SHMOP		"shmop support" ${WITH_SHMOP:-OFF} \
-SNMP		"SNMP support" ${WITH_SNMP:-OFF} \
+SNMP		"SNMP support (implies OPENSSL)" ${WITH_SNMP:-OFF} \
 SOCKETS		"sockets support" ${WITH_SOCKETS:-OFF} \
 SYBASEDB	"Sybase/MS-SQL database support (DB-lib)" ${WITH_SYBASEDB:-OFF} \
 SYBASECT	"Sybase/MS-SQL database support (CT-lib)" ${WITH_SYBASECT:-OFF} \
