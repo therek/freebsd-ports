045c060783f12aaecb48fefdf16cef475714b4c8
daichi <daichi@FreeBSD.org>
2003-08-20 06:33:23 +0000

    update audio/aureal-kmod: broken by busdma commit
    PR:		55513
    Submitted by:	James E. Flemer <jflemer@alum.rpi.edu>

--- audio/aureal-kmod/files/patch-au88x0.c
+++ audio/aureal-kmod/files/patch-au88x0.c
@@ -1,5 +1,5 @@
 
-$FreeBSD: ports/audio/aureal-kmod/files/patch-au88x0.c,v 1.1 2001/01/15 17:42:46 sobomax Exp $
+$FreeBSD: ports/audio/aureal-kmod/files/patch-au88x0.c,v 1.2 2001/02/26 04:19:31 steve Exp $
 
 --- au88x0.c.orig	Mon Jan 15 18:44:49 2001
 +++ au88x0.c	Mon Jan 15 18:44:49 2001
@@ -13,3 +13,16 @@ $FreeBSD: ports/audio/aureal-kmod/files/patch-au88x0.c,v 1.1 2001/01/15 17:42:46
  
  #include <pci/pcireg.h>
  #include <pci/pcivar.h>
+@@ -852,7 +853,11 @@
+ 		/*highaddr*/BUS_SPACE_MAXADDR,
+ 		/*filter*/NULL, /*filterarg*/NULL,
+ 		/*maxsize*/AU_BUFFSIZE, /*nsegments*/1, /*maxsegz*/0x3ffff,
+-		/*flags*/0, &au->parent_dmat) != 0) {
++		/*flags*/0,
++#if __FreeBSD_version > 501102
++		/*lockfunc*/NULL, /*lockarg*/NULL,
++#endif
++                &au->parent_dmat) != 0) {
+ 		device_printf(dev, "unable to create dma tag\n");
+ 		goto bad;
+ 	}
