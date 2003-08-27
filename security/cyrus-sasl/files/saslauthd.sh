0c62199f612aa21a62e06c5eeb30414a0a6ce5f6
marcus <marcus@FreeBSD.org>
2003-08-27 04:38:36 +0000

    Add RC_SUBR support.
    PR:		54116
    Submitted by:	Scot W. Hetzel <hetzels@westbend.net>

--- security/cyrus-sasl/files/saslauthd.sh
+++ security/cyrus-sasl/files/saslauthd.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $FreeBSD: ports/security/cyrus-sasl/files/saslauthd.sh,v 1.2 2003/02/26 11:25:55 edwin Exp $
+# $FreeBSD: ports/security/cyrus-sasl/files/saslauthd.sh,v 1.3 2003/05/09 12:41:21 arved Exp $
 #
 
 # PROVIDE: saslauthd1
@@ -25,72 +25,12 @@ saslauthd1_enable="%%ENABLE_SASLAUTHD%%"		# Enable saslauthd
 #saslauthd1_program="${prefix}/sbin/saslauthd1"	# Location of saslauthd1
 saslauthd1_flags="-a pam"			# Flags to saslauthd program
 
-if [ -f /etc/rc.subr ]; then
-	. /etc/rc.subr
+. %%RC_SUBR%%
 
-	name="saslauthd1"
-	rcvar=`set_rcvar`
-	command="${prefix}/sbin/${name}"
-	pidfile="/var/state/${name}/mux.pid"
+name="saslauthd1"
+rcvar=`set_rcvar`
+command="${prefix}/sbin/${name}"
+pidfile="/var/state/${name}/mux.pid"
 
-	load_rc_config $name
-	run_rc_command "$1"
-else
-	# Suck in the configuration variables.
-	if [ -z "${source_rc_confs_defined}" ]; then
-		if [ -r /etc/defaults/rc.conf ]; then
-			. /etc/defaults/rc.conf
-			source_rc_confs
-		elif [ -r /etc/rc.conf ]; then
-			. /etc/rc.conf
-		fi
-	fi
-
-	if [ -n "${sasl_saslauthd1_enable}" ]; then
-		echo "sasl_saslauthd1_enable has been depreciated, use saslauthd1_enable instead"
-		saslauthd1_enable=$sasl_saslauthd1_enable
-	fi
-
-	if [ -z "${saslauthd1_program}" ]; then
-		saslauthd1_program=${prefix}/sbin/saslauthd1
-	fi
-
-	if [ -n "${sasl_saslauthd1_flags}" ]; then
-        	echo "sasl_saslauthd1_flags has been depreciated, use saslauthd1_flags instead"
-        	saslauthd1_flags=$sasl_saslauthd1_flags
-	fi
-
-	rc=0
-
-	case "${saslauthd1_enable}" in
-	    [Yy][Ee][Ss])
-		case "${1}" in
-
-		    start)
-			if [ -x ${saslauthd1_program} ] ; then
-			    ${saslauthd1_program} ${saslauthd1_flags} \
-				&& echo -n " saslauthd1"
-			fi
-			;;
-
-		    stop)
-			if [ -r /var/state/saslauthd1/mux.pid ]; then
-			    kill `cat /var/state/saslauthd1/mux.pid` && \
-				echo -n " saslauthd1"
-			    rm /var/state/saslauthd1/mux.pid
-			fi
-			;;
-
-		    *)
-			echo "usage: $0 {start|stop}" 1>&2
-			rc=64
-			;;
-		esac
-		;;
-	    *)
-		rc=0
-		;;
-	esac
-
-	exit $rc
-fi
+load_rc_config $name
+run_rc_command "$1"
