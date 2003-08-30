150c2126b06436ba1a3266e35a1738bcfc457eef
ume <ume@FreeBSD.org>
2003-08-30 14:17:22 +0000

    Use USE_RC_SUBR.

--- mail/cyrus-imapd24/files/imapd.sh
+++ mail/cyrus-imapd24/files/imapd.sh
@@ -1,26 +1,37 @@
 #!/bin/sh
 
 # Start or stop cyrus-imapd
-# $FreeBSD: ports/mail/cyrus-imapd2/files/imapd.sh,v 1.4 2001/04/12 19:06:31 ume Exp $
+# $FreeBSD: ports/mail/cyrus-imapd22/files/imapd.sh,v 1.5 2003/02/04 15:55:54 ume Exp $
 
-case "$1" in
-start)
-	if [ -x /usr/local/cyrus/bin/master -a \
-	     -f /usr/local/etc/cyrus.conf -a \
-	     -f /usr/local/etc/imapd.conf ]; then
-		/usr/local/cyrus/bin/master -d
-		echo -n ' imapd'
-	fi
-	;;
-stop)
-	if [ -r /var/run/cyrus-master.pid ]; then
-		kill `cat /var/run/cyrus-master.pid` && echo -n ' imapd'
-		rm /var/run/cyrus-master.pid
-	fi
-	;;
-*)
-	echo "Usage: `basename $0` {start|stop}" >&2
-	;;
-esac
+# PROVIDE: cyrus_imapd
+# REQUIRE: DAEMON
+# BEFORE: mail
+# KEYWORD: FreeBSD shutdown
+#
+# NOTE for FreeBSD 5.0+:
+# If you want this script to start with the base rc scripts
+# move imapd.sh to /etc/rc.d/cyrus_imapd
 
-exit 0
+prefix=%%PREFIX%%
+
+# Define these imapd_* variables in one of these files:
+#	/etc/rc.conf
+#	/etc/rc.conf.local
+#	/etc/rc.conf.d/cyrus_imapd
+#
+# DO NOT CHANGE THESE DEFAULT VALUES HERE 
+#
+cyrus_imapd_enable="YES"				# Enable imapd
+#cyrus_imapd_program="${prefix}/cyrus/bin/master"	# Location of imapd
+cyrus_imapd_flags="-d"				# Flags to imapd program
+
+. %%RC_SUBR%%
+
+name="cyrus_imapd"
+rcvar=`set_rcvar`
+command="${prefix}/cyrus/bin/master"
+pidfile="/var/run/cyrus-master.pid"
+required_files="${prefix}/etc/cyrus.conf ${prefix}/etc/imapd.conf"
+
+load_rc_config $name
+run_rc_command "$1"
