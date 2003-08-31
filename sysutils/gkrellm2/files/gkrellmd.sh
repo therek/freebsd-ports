00c6138c054c0c20ec45c87f3f9bc5b57de471b7
ume <ume@FreeBSD.org>
2003-08-31 10:27:18 +0000

    Use USE_RC_SUBR.

--- sysutils/gkrellm2/files/gkrellmd.sh
+++ sysutils/gkrellm2/files/gkrellmd.sh
@@ -1,24 +1,36 @@
 #!/bin/sh
 
 # Start or stop gkrellmd
-# $FreeBSD: ports/sysutils/gkrellm2/files/gkrellmd.sh,v 1.2 2002/09/06 05:18:39 ume Exp $
+# $FreeBSD: ports/sysutils/gkrellm2/files/gkrellmd.sh,v 1.3 2002/10/12 07:11:25 ume Exp $
 
-PREFIX=%%PREFIX%%
+# PROVIDE: gkrellmd
+# REQUIRE: DAEMON
+# BEFORE: LOGIN
+# KEYWORD: FreeBSD shutdown
+#
+# NOTE for FreeBSD 5.0+:
+# If you want this script to start with the base rc scripts
+# move imapd.sh to /etc/rc.d/gkrellmd
 
-case "$1" in
-start)
-	if [ -x ${PREFIX}/sbin/gkrellmd -a \
-	     -f ${PREFIX}/etc/gkrellmd.conf ]; then
-		${PREFIX}/sbin/gkrellmd -d
-		echo -n ' gkrellmd'
-	fi
-	;;
-stop)
-	killall gkrellmd && echo -n ' gkrellmd'
-	;;
-*)
-	echo "Usage: `basename $0` {start|stop}" >&2
-	;;
-esac
+prefix=%%PREFIX%%
 
-exit 0
+# Define these imapd_* variables in one of these files:
+#	/etc/rc.conf
+#	/etc/rc.conf.local
+#	/etc/rc.conf.d/gkrellmd
+#
+# DO NOT CHANGE THESE DEFAULT VALUES HERE 
+#
+gkrellmd_enable="NO"				# Enable gkrellmd
+#gkrellmd_program="${prefix}/cyrus/bin/master"	# Location of gkrellmd
+gkrellmd_flags="-d"				# Flags to gkrellmd program
+
+. %%RC_SUBR%%
+
+name="gkrellmd"
+rcvar=`set_rcvar`
+command="${prefix}/sbin/${name}"
+required_files="${prefix}/etc/${name}.conf"
+
+load_rc_config $name
+run_rc_command "$1"
