747b19d1298d315c5a7e726ea7209acb2233d821
dinoex <dinoex@FreeBSD.org>
2003-09-03 04:26:36 +0000

    - accept OPENSSLVER 0x0090609fL PR:		56295

--- Mk/bsd.openssl.mk
+++ Mk/bsd.openssl.mk
@@ -2,7 +2,7 @@
 # Date created:		31 May 2002
 # Whom:			dinoex
 #
-# $FreeBSD: ports/Mk/bsd.openssl.mk,v 1.7 2003/08/30 07:26:04 dinoex Exp $
+# $FreeBSD: ports/Mk/bsd.openssl.mk,v 1.8 2003/08/31 03:28:27 dinoex Exp $
 #
 # this substitutes USE_OPENSSL=yes
 # just include this makefile after bsd.ports.pre.mk
@@ -52,7 +52,8 @@ WITH_OPENSSL_PORT=yes
 #	Security: version in base must be 0.9.7a
 OPENSSLVER!=	${AWK} '/OPENSSL_VERSION_NUMBER/ { print $$3 }' \
 		/usr/include/openssl/opensslv.h
-.if ${OPENSSLVER} == 0x0090701fL || ${OPENSSLVER} == 0x0090702fL
+# check for safe versions in the base
+.if ${OPENSSLVER} == 0x0090701fL || ${OPENSSLVER} == 0x0090702fL || ${OPENSSLVER} == 0x0090609fL
 WITH_OPENSSL_BASE=yes
 .endif
 .endif
