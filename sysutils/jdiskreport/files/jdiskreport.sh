fabf808d88912b37ded6774002f358b6a5c857a4
linimon <linimon@FreeBSD.org>
2003-12-09 01:54:11 +0000

    Update to 1.1.2.  Changes:
     o Fixed popup menu position in multi-screen environments
     o Improved support for multi-screen environments
     o Formats numbers and dates using the user's locale
     o Distribution export tables use a fixed size unit
     o Fixed broken mnemonics in view menu
    
    PR:		ports/59836
    Submitted by:	Jonathan Belson <jon@witchspace.com> (maintainer)

--- sysutils/jdiskreport/files/jdiskreport.sh
+++ sysutils/jdiskreport/files/jdiskreport.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
-# $FreeBSD$
+# $FreeBSD: ports/sysutils/jdiskreport/files/jdiskreport.sh,v 1.1 2003/06/17 05:03:22 daichi Exp $
 JDISKDIR="%%JDISKDIR%%"
 JAVAVM="%%JAVAVM%%"
+PORTVERSION="%%PORTVERSION%%"
 
-$JAVAVM -jar "$JDISKDIR/jdiskreport.jar"
+$JAVAVM -jar "$JDISKDIR/jdiskreport-$PORTVERSION.jar"
