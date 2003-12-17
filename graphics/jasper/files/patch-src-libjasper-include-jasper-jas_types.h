95ec354f87b71ff9e44b9ca99fcac06d24eb5422
mi <mi@FreeBSD.org>
2003-12-17 17:20:30 +0000

    Upgrade from 1.700.2 to 1.700.5 (released on Oct 4th).

--- graphics/jasper/files/patch-src-libjasper-include-jasper-jas_types.h
+++ graphics/jasper/files/patch-src-libjasper-include-jasper-jas_types.h
@@ -1,9 +1,8 @@
+$FreeBSD: ports/graphics/jasper/files/patch-src-libjasper-include-jasper-jas_types.h,v 1.4 2003/01/20 10:35:47 sobomax Exp $
 
-$FreeBSD: ports/graphics/jasper/files/patch-src-libjasper-include-jasper-jas_types.h,v 1.3 2003/01/19 20:26:58 sobomax Exp $
-
---- src/libjasper/include/jasper/jas_types.h	2003/01/19 20:08:07	1.1
-+++ src/libjasper/include/jasper/jas_types.h	2003/01/19 20:07:51
-@@ -246,13 +246,13 @@
+--- src/libjasper/include/jasper/jas_types.h	Sat Oct  4 12:57:58 2003
++++ src/libjasper/include/jasper/jas_types.h	Wed Dec 17 10:29:46 2003
+@@ -223,13 +223,13 @@
  #endif
  /**********/
  #if !defined(INT_FAST64_MIN)
@@ -13,9 +12,9 @@ $FreeBSD: ports/graphics/jasper/files/patch-src-libjasper-include-jasper-jas_typ
  #define INT_FAST64_MAX	LLONG_MAX
  #endif
  /**********/
- #if !defined(UINT_FAST64_MIN)
+ #if !defined(UINT_FAST64_MAX)
 -typedef ulonglong uint_fast64_t;
 +typedef unsigned long long uint_fast64_t;
- #define UINT_FAST64_MIN	ULLONG_MIN
  #define UINT_FAST64_MAX	ULLONG_MAX
  #endif
+ /**********/
