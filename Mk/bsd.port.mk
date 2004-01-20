c6885932efb0c5a3882ab90e663da574ef84ee0f
marcus <marcus@FreeBSD.org>
2004-01-20 16:18:33 +0000

    s/sanity-check/check-sanity/ since the latter is the actual target name.
    Submitted by:	rushani
    Pointy hats to: linimon and me

--- Mk/bsd.port.mk
+++ Mk/bsd.port.mk
@@ -1,7 +1,7 @@
 #-*- mode: makefile; tab-width: 4; -*-
 # ex:ts=4
 #
-# $FreeBSD: ports/Mk/bsd.port.mk,v 1.474 2003/11/24 01:08:41 marcus Exp $
+# $FreeBSD: ports/Mk/bsd.port.mk,v 1.475 2004/01/20 09:14:09 marcus Exp $
 #	$NetBSD: $
 #
 #	bsd.port.mk - 940820 Jordan K. Hubbard.
@@ -661,7 +661,7 @@ FreeBSD_MAINTAINER=	portmgr@FreeBSD.org
 #
 # Default sequence for "all" is:
 #
-#     sanity-check fetch checksum extract patch configure build
+#     check-sanity fetch checksum extract patch configure build
 #
 # Please read the comments in the targets section below; you
 # should be able to use the pre-* or post-* targets/scripts
@@ -2796,7 +2796,7 @@ IGNORECMD=	${DO_NADA}
 IGNORECMD=	${ECHO_MSG} "===>  ${PKGNAME} ${IGNORE}."
 .endif
 
-.for target in sanity-check fetch checksum extract patch configure all build install reinstall package
+.for target in check-sanity fetch checksum extract patch configure all build install reinstall package
 ${target}:
 	@${IGNORECMD}
 .if defined(INSTALLS_DEPENDS)
@@ -3758,7 +3758,7 @@ package-message:
 # Empty pre-* and post-* targets
 
 .for stage in pre post
-.for name in sanity-check fetch extract patch configure build install package
+.for name in check-sanity fetch extract patch configure build install package
 
 .if !target(${stage}-${name})
 ${stage}-${name}:
