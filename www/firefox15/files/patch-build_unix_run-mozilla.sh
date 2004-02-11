789022c2b0cc604f6f7d61b6025b876da37402fe
marcus <marcus@FreeBSD.org>
2004-02-11 19:21:02 +0000

    Backuo the previous commit before anyone notices.  I have a big patch coming that will fix this and some other things.  No need to bump PORTREVISION twice.

--- www/firefox15/files/patch-build_unix_run-mozilla.sh
+++ www/firefox15/files/patch-build_unix_run-mozilla.sh
@@ -1,4 +1,4 @@
-$FreeBSD: ports/www/firefox/files/patch-build_unix_run-mozilla.sh,v 1.2 2004/02/11 07:16:17 marcus Exp $
+$FreeBSD: ports/www/firefox/files/patch-build_unix_run-mozilla.sh,v 1.3 2004/02/11 19:05:11 trevor Exp $
 
 --- build/unix/run-mozilla.sh.orig	Mon Feb  9 19:25:14 2004
 +++ build/unix/run-mozilla.sh	Mon Feb  9 21:18:11 2004
@@ -7,7 +7,7 @@ $FreeBSD: ports/www/firefox/files/patch-build_unix_run-mozilla.sh,v 1.2 2004/02/
  ##
  ## Set LD_LIBRARY_PATH
 -LD_LIBRARY_PATH=${MOZ_DIST_BIN}:${MOZ_DIST_BIN}/plugins:${MRE_HOME}${LD_LIBRARY_PATH+":$LD_LIBRARY_PATH"}
-+LD_LIBRARY_PATH=${MOZ_DIST_BIN}:${MOZ_DIST_BIN}/plugins:%%PREFIX%%/lib/browser_plugins:${MRE_HOME}${LD_LIBRARY_PATH+":$LD_LIBRARY_PATH"}
++LD_LIBRARY_PATH=${MOZ_DIST_BIN}:${MOZ_DIST_BIN}/plugins:%PREFIX%%/lib/browser_plugins:${MRE_HOME}${LD_LIBRARY_PATH+":$LD_LIBRARY_PATH"}
  if [ -n "$LD_LIBRARYN32_PATH" ]
  then
  	LD_LIBRARYN32_PATH=${MOZ_DIST_BIN}:${MOZ_DIST_BIN}/plugins:${MRE_HOME}${LD_LIBRARYN32_PATH+":$LD_LIBRARYN32_PATH"}
