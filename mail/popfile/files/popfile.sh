ac894b2222aff7125deea0bc625777515648c02e
matusita <matusita@FreeBSD.org>
2004-03-27 05:19:40 +0000

    Quote variable to avoid unwanted error, even if the pathname includes spaces.  The content is changed but the previous commit is a few minutes before, don't bump PORTREVISION to avoid confusion.

--- mail/popfile/files/popfile.sh
+++ mail/popfile/files/popfile.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 #
-# $FreeBSD: ports/mail/popfile/files/popfile.sh,v 1.1 2004/03/17 13:47:27 matusita Exp $
+# $FreeBSD: ports/mail/popfile/files/popfile.sh,v 1.2 2004/03/27 05:11:21 matusita Exp $
 #
 case "${HOME}" in
 "")
 	export POPFILE_USER_DEFAULT=/var/db/popfile
 	;;
 *)
-	export POPFILE_USER_DEFAULT=${HOME}/.popfile
+	export POPFILE_USER_DEFAULT="${HOME}/.popfile"
 	;;
 esac
 
@@ -20,6 +20,6 @@ die()
 export POPFILE_ROOT=%%DATADIR%%
 export POPFILE_USER=${POPFILE_USER:-${POPFILE_USER_DEFAULT}}
 
-mkdir -p ${POPFILE_USER} || die "mkdir ${POPFILE_USER} failed"
-cd ${POPFILE_USER}
+mkdir -p "${POPFILE_USER}" || die "mkdir ${POPFILE_USER} failed"
+cd "${POPFILE_USER}"
 %%DATADIR%%/popfile.pl >/dev/null 2>&1 &
