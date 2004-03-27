61e290d872e10146ef0b5110f29d037f9635a24c
matusita <matusita@FreeBSD.org>
2004-03-27 05:11:21 +0000

    New features to popfile.sh startup script, including: 	- The default working directory can be tweaked by user; if 	  POPFILE_USER variable is defined before, use it simply. 	- If POPFILE_USER variable is not defined, the script default 	  is either ${HOME}/.popfile (old default) or /var/db/popfile 	  if ${HOME} variable is not defined or defined as null string. 	- If ${POPFILE_USER} directory cannot be made, exit with error.
    Most casual users shouldn't affect this change, however, the script
    itself is changed, so bump PORTREVISION.
    
    Inspired from: http://home.jp.freebsd.org/cgi-bin/showmail/ports-jp/14635
                   (in Japanese)

--- mail/popfile/files/popfile.sh
+++ mail/popfile/files/popfile.sh
@@ -1,10 +1,25 @@
 #!/bin/sh
 #
-# $FreeBSD$
+# $FreeBSD: ports/mail/popfile/files/popfile.sh,v 1.1 2004/03/17 13:47:27 matusita Exp $
 #
-export POPFILE_ROOT=%%DATADIR%%
-export POPFILE_USER=${HOME}/.popfile
+case "${HOME}" in
+"")
+	export POPFILE_USER_DEFAULT=/var/db/popfile
+	;;
+*)
+	export POPFILE_USER_DEFAULT=${HOME}/.popfile
+	;;
+esac
 
-mkdir -p ${POPFILE_USER}
+die()
+{
+	echo "$1"
+	exit 1
+}
+
+export POPFILE_ROOT=%%DATADIR%%
+export POPFILE_USER=${POPFILE_USER:-${POPFILE_USER_DEFAULT}}
+
+mkdir -p ${POPFILE_USER} || die "mkdir ${POPFILE_USER} failed"
 cd ${POPFILE_USER}
 %%DATADIR%%/popfile.pl >/dev/null 2>&1 &
