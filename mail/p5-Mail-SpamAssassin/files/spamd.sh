9b18f215d43a59ddc507635ebb6e3cba78374c99
sf <sf@FreeBSD.org>
2004-03-30 20:21:04 +0000

    unbreak spamd startup. o use ${spamd_pidfile}, instead of ${pidfile}. o ${spamd_pidfile} should be used after its definition.

--- mail/p5-Mail-SpamAssassin/files/spamd.sh
+++ mail/p5-Mail-SpamAssassin/files/spamd.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $FreeBSD: ports/mail/p5-Mail-SpamAssassin/files/spamd.sh,v 1.5 2004/01/13 08:29:28 edwin Exp $
+# $FreeBSD: ports/mail/p5-Mail-SpamAssassin/files/spamd.sh,v 1.6 2004/03/25 16:22:59 mat Exp $
 #
 
 # PROVIDE: spamd
@@ -34,8 +34,8 @@ stop_postcmd()
 # set defaults
 
 spamd_enable=${spamd_enable:-"NO"}
-spamd_flags=${spamd_flags:-"-a -c -d -r ${pidfile}"}
 spamd_pidfile=${spamd_pidfile:-"/var/run/spamd.pid"}
+spamd_flags=${spamd_flags:-"-a -c -d -r ${spamd_pidfile}"}
 
 load_rc_config $name
 run_rc_command "$1"
