fcbcc42552849ec7e704929a5cdf92787e10768a
osa <osa@FreeBSD.org>
2004-05-21 08:39:04 +0000

    Fix signed int problem in display (when displaying very  active flows it is possible to overflow bytecount). [1]
    Also: [2]
     o use DOCSDIR and DATADIR macros
     o move files from pkg-plist to Makefile
     o remove pkg-plist
    
    Reported by:	Gleb Smirnoff <glebius at sell dot sick dot ru>
    Submitted by:	Dmitry Morozovsky <marck at rinet dot ru> (maintainer) [1],
    		osa [2]
    Approved by:	maintainer
    PR:		66936

--- net-mgmt/ehnt/files/patch-ehnt__processflow.c
+++ net-mgmt/ehnt/files/patch-ehnt__processflow.c
@@ -1,8 +1,8 @@
 
-$FreeBSD$
+$FreeBSD: ports/net-mgmt/ehnt/files/patch-ehnt__processflow.c,v 1.1 2002/11/16 14:36:35 edwin Exp $
 
 --- ehnt_processflow.c.orig	Thu Oct  4 22:18:29 2001
-+++ ehnt_processflow.c	Thu Nov 14 22:17:18 2002
++++ ehnt_processflow.c	Fri May 21 11:22:02 2004
 @@ -218,7 +218,7 @@
  	dstip.s_addr=rec->dstaddr;
  	nexthop.s_addr=rec->nexthop;
@@ -12,3 +12,12 @@ $FreeBSD$
  	printf("%d:%s:%d:",
  	       rec->input_index,
  	       inet_ntoa(srcip),
+@@ -228,7 +228,7 @@
+ 	       inet_ntoa(dstip),
+ 	       rec->dstport);
+ 	
+-	printf("%d:%d:%d:",rec->dOctets,rec->dPkts,rec->prot);
++	printf("%u:%d:%d:",rec->dOctets,rec->dPkts,rec->prot);
+ 	printf("%d:%d\n",rec->src_as,rec->dst_as);
+ 
+ 	if (e_flt->big) {
