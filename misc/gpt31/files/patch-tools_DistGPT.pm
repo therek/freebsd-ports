f663dda8752761557baf1f7e03d314596bc60c13
brooks <brooks@FreeBSD.org>
2004-06-18 18:51:02 +0000

    - misc/gpt31 has been repocopied from misc/gpt - Add CONFILCTS and NO_LATEST_LINK

--- misc/gpt31/files/patch-tools_DistGPT.pm
+++ misc/gpt31/files/patch-tools_DistGPT.pm
@@ -1,5 +1,5 @@
 
-$FreeBSD$
+$FreeBSD: ports/misc/gpt31/files/patch-tools_DistGPT.pm,v 1.1 2004/02/26 19:44:13 krion Exp $
 
 --- tools/DistGPT.pm.orig	Fri Jan 16 16:31:06 2004
 +++ tools/DistGPT.pm	Fri Jan 16 16:34:24 2004
