7b037970fe8128e43827dacb50aaee5e2a854e56
osa <osa@FreeBSD.org>
2004-06-23 12:01:33 +0000

    Make small improvement: print packets per second in topmode.
    Original patch from:		Gleb Smirnoff <glebius@cell.sick.ru>
    Fixed patch from, approved by:	Dmitry Morozovsky <marck@rinet.ru> (maintainer)

--- net-mgmt/ehnt/files/patch-ehnt.h
+++ net-mgmt/ehnt/files/patch-ehnt.h
@@ -1,9 +1,17 @@
 
-$FreeBSD$
+$FreeBSD: ports/net-mgmt/ehnt/files/patch-ehnt.h,v 1.1 2004/05/21 08:39:04 osa Exp $
 
 --- ehnt.h.orig	Thu Oct  4 22:18:29 2001
-+++ ehnt.h	Thu May 20 16:07:24 2004
-@@ -93,7 +93,7 @@
++++ ehnt.h	Wed Jun 23 14:35:08 2004
+@@ -59,6 +59,7 @@
+ 
+ struct ehnt_stats {
+   double InBoundTotal, OutBoundTotal;
++  double InBoundTotalPkts, OutBoundTotalPkts;
+   double *InBoundSrcAS, *InBoundDstAS, *OutBoundSrcAS, *OutBoundDstAS;
+   double *InBoundProto, *OutBoundProto;
+   double *InBoundSrcTcpPort, *InBoundDstTcpPort, 
+@@ -93,7 +94,7 @@
  int ProcessFlow(struct flow_ver5_rec *, struct in_addr *, struct ehnt_struct *);
  int Init_ASN_Lookups(void);
  char * ASN_Lookup(int);
