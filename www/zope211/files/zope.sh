47d94208439d00b4b76c951fb238113aee4d5bbe
thierry <thierry@FreeBSD.org>
2004-07-02 20:41:00 +0000

    Upgrade to 2.7.1.
    PR:		ports/68408
    Submitted by:	HAYASHI Yasushi
    Approved by:	maintainer.

--- www/zope211/files/zope.sh
+++ www/zope211/files/zope.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Start or stop zope
-# $FreeBSD: ports/www/zope/files/zope.sh,v 1.1 2004/04/20 14:18:32 perky Exp $
+# $FreeBSD: ports/www/zope/files/zope.sh,v 1.2 2004/04/20 14:29:26 perky Exp $
 
 # PROVIDE: zope
 # REQUIRE: DAEMON
@@ -18,7 +18,7 @@ prefix=%%PREFIX%%
 # DO NOT CHANGE THESE DEFAULT VALUES HERE
 #
 zope_enable=${zope_enable:-"NO"}        # Enable gkrellmd
-zope_instances=${zope_instances:-""}    # List of instancehome dirs 
+zope_instances=${zope_instances:-""}    # List of instancehome dirs
 
 . %%RC_SUBR%%
 
@@ -47,7 +47,7 @@ if checkyesno zope_enable; then
 	do
 		if [ -r ${instance}/etc/${name}.conf -a -x ${instance}/bin/zopectl ]; then
 			echo -n "       Instance ${instance} -> "
-			${instance}/bin/zopectl $1                           
+			${instance}/bin/zopectl $1
 		fi
 	done
 fi
