502a9fe08ce97f6ee7c319b6bc30ffb2f1539eb4
adamw <adamw@FreeBSD.org>
2004-07-04 23:02:22 +0000

    Chase libgsf* shared library.

--- Mk/bsd.gnome.mk
+++ Mk/bsd.gnome.mk
@@ -1,7 +1,7 @@
 #-*- mode: Fundamental; tab-width: 4; -*-
 # ex:ts=4
 #
-# $FreeBSD: ports/Mk/bsd.gnome.mk,v 1.71 2004/06/26 22:08:40 adamw Exp $
+# $FreeBSD: ports/Mk/bsd.gnome.mk,v 1.72 2004/06/27 17:32:29 marcus Exp $
 #	$NetBSD: $
 #
 # Please view me with 4 column tabs!
@@ -382,11 +382,11 @@ pkgconfig_DETECT=			${LOCALBASE}/bin/pkg-config
 pkgconfig_BUILD_DEPENDS=	pkg-config:${PORTSDIR}/devel/pkgconfig
 pkgconfig_RUN_DEPENDS=		pkg-config:${PORTSDIR}/devel/pkgconfig
 
-libgsf_LIB_DEPENDS=			gsf-1.10:${PORTSDIR}/devel/libgsf
+libgsf_LIB_DEPENDS=			gsf-1.11:${PORTSDIR}/devel/libgsf
 libgsf_DETECT=			${LOCALBASE}/libdata/pkgconfig/libgsf-1.pc
 libgsf_USE_GNOME_IMPL=		glib20 libxml2
 
-libgsf_gnome_LIB_DEPENDS=	gsf-gnome-1.10:${PORTSDIR}/devel/libgsf-gnome
+libgsf_gnome_LIB_DEPENDS=	gsf-gnome-1.11:${PORTSDIR}/devel/libgsf-gnome
 libgsf_gnome_DETECT=		${LOCALBASE}/libdata/pkgconfig/libgsf-gnome-1.pc
 libgsf_gnome_USE_GNOME_IMPL=	libgsf gnomevfs2
 
