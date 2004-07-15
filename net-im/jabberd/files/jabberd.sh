d8e56c93c64f6c02b06e25dcf537e3550998476f
vs <vs@FreeBSD.org>
2004-07-15 10:46:00 +0000

    Fix hardcoded paths.
    PR:		ports/69082
    Submitted by:	Kirk Strauser (maintainer)

--- net-im/jabberd/files/jabberd.sh
+++ net-im/jabberd/files/jabberd.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
 
 # Start or stop jabberd
-# $FreeBSD: $
+# $FreeBSD: ports/net/jabberd/files/jabberd.sh,v 1.3 2004/07/13 02:38:15 edwin Exp $
 
 # PROVIDE: jabberd
 # REQUIRE: DAEMON
 # BEFORE: LOGIN
 # KEYWORD: FreeBSD shutdown
 #
-prefix=/usr/local
+prefix=%%PREFIX%%
 
 # Define these jabberd_* variables in one of these files:
 #       /etc/rc.conf
 #       /etc/rc.conf.local
-#       /etc/rc.conf.d/gkrellmd
 #
 # DO NOT CHANGE THESE DEFAULT VALUES HERE
 #
@@ -22,13 +21,13 @@ jabberd_enable="NO"
 jabberd_flags=""
 jabberd_user="jabber"
 
-. /etc/rc.subr
+. %%RC_SUBR%%
 
 name="jabberd"
 rcvar=`set_rcvar`
-command="/usr/local/bin/jabberd"
+command="${prefix}/bin/jabberd"
 command_args="&"
-procname="/usr/local/bin/router"
+procname="${prefix}/bin/router"
 
 load_rc_config $name
 
