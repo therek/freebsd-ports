df977d353d6309726488d3bf7f5518b7aea18086
vs <vs@FreeBSD.org>
2004-08-27 12:06:50 +0000

    - Update to 0.43 - Fix packaging
    PR:		ports/70597
    Submitted by:	Ronald Klop (maintainer)

--- java/jmp/files/patch-instance_owners.c
+++ java/jmp/files/patch-instance_owners.c
@@ -1,14 +1,10 @@
-$FreeBSD: ports/java/jmp/files/patch-instance_owners.c,v 1.1 2003/05/15 16:34:36 glewis Exp $
-
---- instance_owners.c.orig	Fri Jun 25 02:20:42 2004
-+++ instance_owners.c	Fri Jun 25 02:28:35 2004
+--- /tmp/instance_owners.c	Wed Aug 18 20:23:20 2004
++++ instance_owners.c	Wed Aug 18 20:24:24 2004
 @@ -16,0 +17,4 @@
 +#if __FreeBSD__ < 5
 +#define PRId32 "d"
 +#endif
 +
-@@ -205,0 +210 @@
-+    GtkWidget* menuitem;
-@@ -207 +212 @@
--    GtkWidget* menuitem = gtk_menu_item_new_with_label (buf);
+@@ -207 +211 @@
+-    omenu = gtk_menu_new ();
 +    menuitem = gtk_menu_item_new_with_label (buf);
