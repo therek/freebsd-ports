d6729b02d68d27c6610c7a670e6149121e0e0d2f
sergei <sergei@FreeBSD.org>
2004-10-15 16:17:23 +0000

    - Update to 0.7.4:   - Added German and Russian translations   - Configuration file changes
    PR:		ports/72735
    Submitted by:	Janos Mohacsi (maintainer)
    
    While I'm here:
    - Move examples installation out of NOPORTDOCS check
    - Add CONFIGURE_TARGET to silense configure warning
    - Minor Makefile tweaks

--- comms/scmxx/files/patch-src_charsets_ucs4__dec.c
+++ comms/scmxx/files/patch-src_charsets_ucs4__dec.c
@@ -1,9 +1,9 @@
 
-$FreeBSD$
+$FreeBSD: ports/comms/scmxx/files/patch-src_charsets_ucs4__dec.c,v 1.1 2004/07/23 08:03:47 arved Exp $
 
---- src/charsets/ucs4_dec.c.orig	Thu Jul 22 18:16:40 2004
-+++ src/charsets/ucs4_dec.c	Thu Jul 22 18:20:04 2004
-@@ -19,6 +19,12 @@
+--- src/charsets/ucs4_dec.c.orig
++++ src/charsets/ucs4_dec.c
+@@ -21,6 +21,12 @@
  #include <errno.h>
  #include <iconv.h>
  
@@ -13,6 +13,6 @@ $FreeBSD$
 +#define UINT16_MAX USHRT_MAX
 +#endif
 +
- #include <config.h>
- #ifndef ICONV_CAST
- #  define ICONV_CAST char**
+ size_t replace_char_escape (char **inbuf, size_t *inbytesleft,
+                             char **outbuf, size_t *outbytesleft)
+ {
