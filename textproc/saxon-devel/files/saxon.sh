72388e71eb93168f23edbc86210b5f7e6defb836
hq <hq@FreeBSD.org>
2004-11-09 16:03:39 +0000

    - Removed unnecessary run dependency on Xerces-J (given XML APIs and some XML   parser are included in JDK 1.4+ Standard API) - Tweaked CLASSPATH in launcher script: Only Saxon installed JARs are indeed   required for command-line operations (see above) - Added some more "" in launcher script to ensure spaces in paths and args will   not break anything

--- textproc/saxon-devel/files/saxon.sh
+++ textproc/saxon-devel/files/saxon.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $FreeBSD: ports/textproc/saxon-devel/files/saxon.sh,v 1.2 2004/07/17 08:19:44 glewis Exp $
+# $FreeBSD: ports/textproc/saxon-devel/files/saxon.sh,v 1.3 2004/10/27 16:50:43 hq Exp $
 
 IAM=`basename "$0"`
 
@@ -11,4 +11,10 @@ else
 	LAUNCHER_CLASS="net.sf.saxon.Transform"
 fi
 
-JAVAVM=%%JAVAVM%% %%LOCALBASE%%/bin/javavm -classpath "`%%LOCALBASE%%/bin/classpath`" ${LAUNCHER_CLASS} $@
+SAXON_CLASSPATH=""
+for jarfile in %%SAXON_JARS%%
+do
+	SAXON_CLASSPATH="${SAXON_CLASSPATH}:${jarfile}"
+done
+
+JAVAVM="%%JAVAVM%%" "%%LOCALBASE%%/bin/javavm" -classpath "${SAXON_CLASSPATH}" "${LAUNCHER_CLASS}" "$@"
