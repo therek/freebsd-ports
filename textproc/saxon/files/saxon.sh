e7f7596ff480a9dcda821986088c0bd3b116b508
hq <hq@FreeBSD.org>
2004-11-10 02:37:24 +0000

    - Tweaked CLASSPATH in launcher script: Only Saxon installed JARs are indeed   required (given XML APIs and the Aelfred XML parser are included in   saxon.jar) - Added some more "" in launcher script to ensure spaces in paths and args will   not break anything - "${CP} -r" -> "${FIND} -exec ${INSTALL_DATA}" for samples and docs - Takeover maintainership

--- textproc/saxon/files/saxon.sh
+++ textproc/saxon/files/saxon.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 #
-# $FreeBSD$
+# $FreeBSD: ports/textproc/saxon/files/saxon.sh,v 1.1 2004/07/02 19:30:27 glewis Exp $
 
-export CLASSPATH=`%%PREFIX%%/bin/classpath`
-javavm -jar %%JAVAJARDIR%%/saxon.jar $@
+SAXON_CLASSPATH=""
+for jarfile in %%SAXON_JARS%%
+do
+	SAXON_CLASSPATH="${SAXON_CLASSPATH}:${jarfile}"
+done
+
+"%%LOCALBASE%%/bin/javavm" -classpath "${SAXON_CLASSPATH}" com.icl.saxon.StyleSheet "$@"
