906ba8999b97451880b2095871d9d65e852416c6
lofi <lofi@FreeBSD.org>
2004-12-02 00:15:44 +0000

    Sync with ports.

--- Mk/bsd.kde4.mk
+++ Mk/bsd.kde4.mk
@@ -1,7 +1,7 @@
 #-*- mode: Makefile; tab-width: 4; -*-
 # ex:ts=4
 #
-# $FreeBSD: ports/Mk/bsd.kde.mk,v 1.41 2004/08/30 19:55:08 lofi Exp $
+# $FreeBSD: ports/Mk/bsd.kde.mk,v 1.42 2004/11/08 00:33:37 lofi Exp $
 #
 # Please view me with 4 column tabs!
 
@@ -115,7 +115,7 @@ CONFIGURE_ENV+=	MOC="${MOC}" CPPFLAGS="${CPPFLAGS} ${QTCPPFLAGS}" LIBS="${QTCFGL
 
 # Yeah, it's namespace pollution, but this is really the best place for this
 # stuff. Arts does NOT use it anymore.
-KDE_VERSION=		3.3.1
+KDE_VERSION=		3.3.2
 KDE_ORIGVER=	${KDE_VERSION}
 KDE_PREFIX?=	${LOCALBASE}
 
