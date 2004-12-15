b1a66ec110df60955e923f8b359049cf08130776
vs <vs@FreeBSD.org>
2004-12-15 08:04:56 +0000

    Fix path to scanlogd in rc-script (bump PORTREV)
    PR:		ports/75034
    Submitted by:	Hilko Meyer
    Approved by:	maintainer

--- security/scanlogd/files/scanlogd.sh
+++ security/scanlogd/files/scanlogd.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Start or stop scanlogd
-# $FreeBSD$
+# $FreeBSD: ports/security/scanlogd/files/scanlogd.sh,v 1.1 2004/12/13 10:07:13 krion Exp $
 
 # PROVIDE: scanlogd
 # REQUIRE: DAEMON
@@ -12,14 +12,14 @@
 prefix=%%PREFIX%%
 
 scanlogd_enable=${scanlogd_enable:-"NO"}	# Enable scanlogd
-scanlogd_program="${prefix}/sbin/scanlogd"	# Location of scanlogd
+scanlogd_program="${prefix}/bin/scanlogd"	# Location of scanlogd
 scanlogd_flags=${scanlogd_flags:-""}		# Flags to scanlogd
 
 . %%RC_SUBR%%
 
 name="scanlogd"
 rcvar=`set_rcvar`
-command="${prefix}/sbin/${name}"
+command="${prefix}/bin/${name}"
 required_files=""
 
 load_rc_config $name
