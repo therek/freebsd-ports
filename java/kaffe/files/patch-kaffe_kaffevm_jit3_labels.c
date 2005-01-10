3112293b76b5aca8c162765f46b33f9b5df69222
edwin <edwin@FreeBSD.org>
2005-01-10 11:04:51 +0000

    Move java/kaffe-devel to java/kaffe Remove java/kaffe-devel
    Submitted by:	maintainer (via irc)

--- java/kaffe/files/patch-kaffe_kaffevm_jit3_labels.c
+++ java/kaffe/files/patch-kaffe_kaffevm_jit3_labels.c
@@ -1,5 +1,5 @@
 
-$FreeBSD$
+$FreeBSD: ports/java/kaffe-devel/files/patch-kaffe_kaffevm_jit3_labels.c,v 1.1 2005/01/09 00:20:46 krion Exp $
 
 --- kaffe/kaffevm/jit3/labels.c.orig
 +++ kaffe/kaffevm/jit3/labels.c
