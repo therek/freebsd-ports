b0462ab15ba804c460702ca617e91a473b7dbc19
edwin <edwin@FreeBSD.org>
2005-01-12 22:02:20 +0000

    [maintainer-update] fix x11/login.app build error
    	Does two unrelated things:
    	1) add linker flag to fix build [untested]
    	2) fix startup script [tested]
    
    	Whether 1) will work I have no idea. On my own machine,
    	everything works all right, and the errorlogs are
    	confusing. 2) is needed.
    
    PR:		ports/76171
    Submitted by:	Tobias Roth <ports@fsck.ch>

--- x11/login.app/files/loginapp.sh
+++ x11/login.app/files/loginapp.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $FreeBSD$
+# $FreeBSD: ports/x11/login.app/files/loginapp.sh,v 1.1 2005/01/10 10:51:29 edwin Exp $
 #
 
 # PROVIDE: login_app
@@ -19,6 +19,7 @@ login_app_enable=${login_app_enable-"NO"}
 name="login_app"
 rcvar=`set_rcvar`
 command="%%X11BASE%%/GNUstep/Apps/Login.app/Login"
+command_args="&"
 
 load_rc_config $name
 run_rc_command "$1"
