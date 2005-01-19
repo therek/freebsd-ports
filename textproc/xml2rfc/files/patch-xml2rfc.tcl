8d0992e5aedb4fd7804faab8200253094d834cc6
edwin <edwin@FreeBSD.org>
2005-01-19 21:16:14 +0000

    [Maintainer patch] Update textproc/xml2rfc to version 1.28
    	This patch updates textproc/xml2rfc to version 1.28, just
    	released.  It provides a number of formatting changes and
    	other improvements.
    
    	The patch also updates the maintainer address in pkg-descr,
    	which I'd forgotten to do when I took over maintaining this
    	port.
    
    PR:		ports/76375
    Submitted by:	Jonathan Lennox <lennox@cnr.cs.columbia.edu>

--- textproc/xml2rfc/files/patch-xml2rfc.tcl
+++ textproc/xml2rfc/files/patch-xml2rfc.tcl
@@ -1,13 +1,13 @@
 
-$FreeBSD: ports/textproc/xml2rfc/files/patch-xml2rfc.tcl,v 1.1 2002/07/25 19:39:48 naddy Exp $
+$FreeBSD: ports/textproc/xml2rfc/files/patch-xml2rfc.tcl,v 1.2 2004/08/17 08:51:19 pav Exp $
 
 --- xml2rfc.tcl.orig	Mon Aug 16 17:55:38 2004
 +++ xml2rfc.tcl	Mon Aug 16 17:55:45 2004
 @@ -1,6 +1,6 @@
  #!/bin/sh
  # the next line restarts using the correct interpreter \
--exec tclsh "$0" "$0" "$@"
-+exec tclsh8.3 "$0" "$0" "$@"
+-LC_ALL=C exec tclsh "$0" "$0" "$@"
++LC_ALL=C exec tclsh8.3 "$0" "$0" "$@"
  
  
- # NOTE FROM CLIVE
+ #
