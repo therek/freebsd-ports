4f129d1f9eebfb7283f517364ebf6c154f2d5da5
hq <hq@FreeBSD.org>
2005-01-29 00:01:49 +0000

    - Update to bsd.java.mk 2.0 - Fix dynamic plist creation where all JAVASHAREDIR would be listed (and thus   removed with the port) (!) - Use SUB_LIST and tweak the launcher shell script - Move displayed information into pkg-message - Use DATADIR=${JAVASHAREDIR}/${PORTNAME}

--- editors/jedit/files/jedit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# $FreeBSD: ports/editors/jedit/files/jedit.sh,v 1.4 2001/04/23 10:39:43 sobomax Exp $
-
-# Java heap size, in megabytes
-JAVA_HEAP_SIZE=32
-
-exec %%LOCALBASE%%/bin/javavm -mx${JAVA_HEAP_SIZE}m ${JEDIT} -jar \
-	"%%PREFIX%%/share/java/jedit/jedit.jar" "$@"
