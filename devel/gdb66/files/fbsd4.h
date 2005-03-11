622f05bad8812e0c614be987b4f85a839e6405bc
obrien <obrien@FreeBSD.org>
2005-03-11 04:52:49 +0000

    Commit a partial WIP before I loose it.

--- devel/gdb66/files/fbsd4.h
+++ devel/gdb66/files/fbsd4.h
@@ -1,4 +1,4 @@
-// $FreeBSD$
+// $FreeBSD: ports/devel/gdb6/files/fbsd4.h,v 1.1 2004/10/27 18:37:40 obrien Exp $
 
 extern void _rl_set_screen_size (int, int);
 
@@ -13,3 +13,9 @@ rl_get_screen_size (int *rows, int *cols)
   if (rows) *rows = screenheight;
   if (cols) *cols = screenwidth;
 }
+
+#if 0
+#define	savestring
+#include <readline/readline.h>
+#undef	savestring
+#endif
