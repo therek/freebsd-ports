ca28277351b2cc0e9818ea79d3ba87cd614ac324
ale <ale@FreeBSD.org>
2005-04-04 09:20:21 +0000

    Update PHP ports to 4.3.11 and 5.0.4 releases: - fix security issues - unbreak PEAR packages [1]
    PR:		ports/78516 [1]
    Submitted by:	Yen-Ming Lee <leeym@utopia.leeym.com> [1]
    Tested by:	thierry

--- Mk/bsd.php.mk
+++ Mk/bsd.php.mk
@@ -7,7 +7,7 @@
 # Please send all suggested changes to the maintainer instead of committing
 # them to CVS yourself.
 #
-# $FreeBSD: ports/Mk/bsd.php.mk,v 1.13 2005/03/11 16:53:44 pav Exp $
+# $FreeBSD: ports/Mk/bsd.php.mk,v 1.14 2005/03/14 14:37:45 ale Exp $
 #
 # Adding 'USE_PHP=yes' to a port includes this Makefile after bsd.ports.pre.mk.
 # If the port requires a predefined set of PHP extensions, they can be
@@ -167,7 +167,7 @@ PLIST_SUB+=	PHP_EXT_DIR=${PHP_EXT_DIR}
 
 .if defined(USE_PHPIZE) || defined(USE_PHPEXT)
 BUILD_DEPENDS+=	phpize:${PHP_PORT}
-USE_LIBTOOL_VER=	13
+GNU_CONFIGURE=	YES
 WANT_AUTOMAKE_VER=	15
 WANT_AUTOCONF_VER=	253
 CONFIGURE_ARGS+=--with-php-config=${LOCALBASE}/bin/php-config
