71dc5f60c24360c44c991610ee29dfb1118b5988
fenner <fenner@FreeBSD.org>
2005-04-06 22:11:47 +0000

    [Maintainer Update] Update to 1.29.
    This version implements the new version of the required IETF boilerplate
    (RFC 3978), which will become mandatory for Internet-Drafts on May 6.
    
    PR:		ports/79603
    Submitted by:	Jonathan Lennox <lennox@cnr.cs.columbia.edu>

--- textproc/xml2rfc/files/patch-xml2rfc.tcl
+++ textproc/xml2rfc/files/patch-xml2rfc.tcl
@@ -1,13 +1,13 @@
 
-$FreeBSD: ports/textproc/xml2rfc/files/patch-xml2rfc.tcl,v 1.2 2004/08/17 08:51:19 pav Exp $
+$FreeBSD: ports/textproc/xml2rfc/files/patch-xml2rfc.tcl,v 1.3 2005/01/19 21:16:14 edwin Exp $
 
 --- xml2rfc.tcl.orig	Mon Aug 16 17:55:38 2004
 +++ xml2rfc.tcl	Mon Aug 16 17:55:45 2004
 @@ -1,6 +1,6 @@
  #!/bin/sh
  # the next line restarts using the correct interpreter \
--LC_ALL=C exec tclsh "$0" "$0" "$@"
-+LC_ALL=C exec tclsh8.3 "$0" "$0" "$@"
+-exec tclsh "$0" "$0" "$@"
++exec tclsh8.3 "$0" "$0" "$@"
  
  
  #
