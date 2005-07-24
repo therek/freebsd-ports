e5d9d9f21a2e4b1bdd4ada22b6cb89ffeb40a2d8
pav <pav@FreeBSD.org>
2005-07-24 04:00:04 +0000

    - Refresh list of Sourceforge mirrors

--- Mk/bsd.sites.mk
+++ Mk/bsd.sites.mk
@@ -12,7 +12,7 @@
 # be punished accordingly.  DO NOT COMMIT SOMETHING YOU HAVEN'T TESTED.
 # You have been warned. :)
 #
-# $FreeBSD: ports/Mk/bsd.sites.mk,v 1.294 2005/05/29 12:26:41 lioux Exp $
+# $FreeBSD: ports/Mk/bsd.sites.mk,v 1.295 2005/06/05 11:49:30 sem Exp $
 #
 
 # Where to put distfiles that don't have any other master site
@@ -1115,7 +1115,7 @@ MASTER_SITE_SAVANNAH+= \
 .endif
 
 .if !defined(IGNORE_MASTER_SITE_SOURCEFORGE)
-.for mirror in heanet aleron umn belnet nchc voxel optusnet kent unc ufpr
+.for mirror in heanet aleron nchc voxel optusnet kent unc ufpr surfnet mesh
 MASTER_SITE_SOURCEFORGE+= \
 	http://${mirror}.dl.sourceforge.net/sourceforge/%SUBDIR%/
 .endfor
@@ -1124,7 +1124,7 @@ MASTER_SITE_SOURCEFORGE+= \
 # official sf.net mirrors that don't mirror all projects, check
 # http://prdownloads.sourceforge.net/%SUBDIR%/
 .if !defined(IGNORE_MASTER_SITE_SOURCEFORGE_EXTENDED)
-.for mirror in easynews switch puzzle ovh peterhost citkit # keihanna is mostly unreachable
+.for mirror in easynews switch puzzle ovh peterhost citkit keihanna
 MASTER_SITE_SOURCEFORGE_EXTENDED+= \
 	http://${mirror}.dl.sourceforge.net/sourceforge/%SUBDIR%/
 .endfor
