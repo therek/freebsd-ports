15965a57e4be46627c428c91ab6f45c6efb90fdf
dinoex <dinoex@FreeBSD.org>
2005-11-08 08:31:20 +0000

    - RELENG_6_0 check limited to port PR:		88645 Submitted by:	NIIMI Satoshi

--- Mk/bsd.openssl.mk
+++ Mk/bsd.openssl.mk
@@ -2,7 +2,7 @@
 # Date created:		31 May 2002
 # Whom:			dinoex
 #
-# $FreeBSD: ports/Mk/bsd.openssl.mk,v 1.24 2005/10/14 06:41:49 dinoex Exp $
+# $FreeBSD: ports/Mk/bsd.openssl.mk,v 1.25 2005/11/07 20:58:17 dinoex Exp $
 #
 # Use of 'USE_OPENSSL=yes' includes this Makefile after bsd.ports.pre.mk
 #
@@ -46,9 +46,11 @@ WITH_OPENSSL_PORT=yes
 WITH_OPENSSL_STABLE=yes
 .endif
 
+.if defined(WITH_OPENSSL_PORT)
 .if !defined(WITHOUT_OPENSSL_097) && !defined(WITH_OPENSSL_BETA)
 .if ( ${OSVERSION} >= 600000 ) && ( ${OSVERSION} < 600100 )
 WITH_OPENSSL_STABLE=yes
+.endif
 .endif
 .endif
 
