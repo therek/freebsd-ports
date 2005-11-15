fc90ffe69d8cf109a71f0deb65d8d388b08b2ad7
ade <ade@FreeBSD.org>
2005-11-15 06:52:12 +0000

    Mass-conversion to the USE_AUTOTOOLS New World Order.  The code present in bsd.autotools.mk essentially makes this a no-op given that all the old variables set a USE_AUTOTOOLS_COMPAT variable, which is parsed in exactly the same way as USE_AUTOTOOLS itself.
    Moreover, USE_AUTOTOOLS has already been extensively tested by the GNOME
    team -- all GNOME 2.12.x ports use it.
    
    Preliminary documentation can be found at:
    	http://people.FreeBSD.org/~ade/autotools.txt
    
    which is in the process of being SGMLized before introduction into the
    Porters Handbook.
    
    Light blue touch-paper.  Run.

--- Mk/bsd.php.mk
+++ Mk/bsd.php.mk
@@ -7,7 +7,7 @@
 # Please send all suggested changes to the maintainer instead of committing
 # them to CVS yourself.
 #
-# $FreeBSD: ports/Mk/bsd.php.mk,v 1.16 2005/07/13 19:56:27 ale Exp $
+# $FreeBSD: ports/Mk/bsd.php.mk,v 1.17 2005/09/08 08:34:40 ale Exp $
 #
 # Adding 'USE_PHP=yes' to a port includes this Makefile after bsd.ports.pre.mk.
 # If the port requires a predefined set of PHP extensions, they can be
@@ -168,7 +168,7 @@ PLIST_SUB+=	PHP_EXT_DIR=${PHP_EXT_DIR}
 .if defined(USE_PHPIZE) || defined(USE_PHPEXT)
 BUILD_DEPENDS+=	phpize:${PHP_PORT}
 GNU_CONFIGURE=	YES
-WANT_AUTOCONF_VER=	259
+USE_AUTOTOOLS+=	autoconf:259:env
 CONFIGURE_ARGS+=--with-php-config=${LOCALBASE}/bin/php-config
 
 configure-message: phpize-message do-phpize
