cddfaa1bf0fab98bf6c22c2ed0209cfb5f8b14ac
marcus <marcus@FreeBSD.org>
2006-01-24 23:37:03 +0000

    Update to 3.11.
    PR:		92017
    Submitted by:	mi

--- security/nss/files/patch-cmd::platlibs.mk
+++ security/nss/files/patch-cmd::platlibs.mk
@@ -1,5 +1,5 @@
 
-$FreeBSD: ports/security/nss/files/patch-cmd::platlibs.mk,v 1.1 2002/08/08 19:05:41 sobomax Exp $
+$FreeBSD: ports/security/nss/files/patch-cmd::platlibs.mk,v 1.2 2005/07/31 00:06:49 marcus Exp $
 
 --- cmd/platlibs.mk	Wed Apr  6 17:35:41 2005
 +++ cmd/platlibs.mk	Wed Jul 27 22:22:59 2005
@@ -9,43 +9,9 @@ $FreeBSD: ports/security/nss/files/patch-cmd::platlibs.mk,v 1.1 2002/08/08 19:05
 -	$(DIST)/lib/$(LIB_PREFIX)dbm.$(LIB_SUFFIX) \
  	$(DIST)/lib/$(NSPR31_LIB_PREFIX)plc4.$(LIB_SUFFIX) \
  	$(DIST)/lib/$(NSPR31_LIB_PREFIX)plds4.$(LIB_SUFFIX) \
-@@ -85,5 +84,4 @@
- 
- JAR_LIBS = $(DIST)/lib/$(LIB_PREFIX)jar.$(LIB_SUFFIX) \
--	$(DIST)/lib/$(LIB_PREFIX)zlib.$(LIB_SUFFIX) \
- 	$(NULL)
- else
 @@ -120,5 +118,4 @@
  	$(CRYPTOLIB) \
  	$(DIST)/lib/$(LIB_PREFIX)secutil.$(LIB_SUFFIX) \
 -	$(DIST)/lib/$(LIB_PREFIX)dbm.$(LIB_SUFFIX) \
  	$(NULL)
  
-@@ -141,4 +138,5 @@
- 	-lplds4 \
- 	-lnspr4 \
-+	-lz \
- 	$(NULL)
- endif
-@@ -146,5 +144,4 @@
- 
- JAR_LIBS = $(DIST)/lib/$(LIB_PREFIX)jar.$(LIB_SUFFIX) \
--	$(DIST)/lib/$(LIB_PREFIX)zlib.$(LIB_SUFFIX) \
- 	$(NULL)
- 
-@@ -171,5 +168,4 @@
- 
- JAR_LIBS = $(DIST)/lib/$(LIB_PREFIX)jar.$(LIB_SUFFIX) \
--	$(DIST)/lib/$(LIB_PREFIX)zlib.$(LIB_SUFFIX) \
- 	$(NULL)
- else
-@@ -226,8 +222,8 @@
- 	-lplds4 \
- 	-lnspr4 \
-+	-lz \
- 	$(NULL)
- 
- JAR_LIBS = $(DIST)/lib/$(LIB_PREFIX)jar.$(LIB_SUFFIX) \
--	$(DIST)/lib/$(LIB_PREFIX)zlib.$(LIB_SUFFIX) \
- 	$(NULL)
- endif
