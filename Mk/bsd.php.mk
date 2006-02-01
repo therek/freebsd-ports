4d056ba5d396a10f1c1d226a29d78254ded1efef
ale <ale@FreeBSD.org>
2006-02-01 07:18:47 +0000

    - pfpro extension is only for php4 - add location to pfpro directory

--- Mk/bsd.php.mk
+++ Mk/bsd.php.mk
@@ -7,7 +7,7 @@
 # Please send all suggested changes to the maintainer instead of committing
 # them to CVS yourself.
 #
-# $FreeBSD: ports/Mk/bsd.php.mk,v 1.23 2006/01/21 17:37:01 krion Exp $
+# $FreeBSD: ports/Mk/bsd.php.mk,v 1.24 2006/01/31 23:00:20 pav Exp $
 #
 # Adding 'USE_PHP=yes' to a port includes this Makefile after bsd.ports.pre.mk.
 # If the port requires a predefined set of PHP extensions, they can be
@@ -254,13 +254,13 @@ _USE_PHP_ALL=	bcmath bz2 calendar ctype curl dba dbase \
 		exif fileinfo filepro fribidi ftp gd gettext gmp \
 		iconv imagick imap interbase ldap mbstring mcrypt \
 		mhash ming mssql mysql ncurses odbc \
-		openssl panda pcntl pcre pdf pfpro pgsql posix \
+		openssl panda pcntl pcre pdf pgsql posix \
 		pspell radius readline recode session shmop snmp \
 		sockets sybase_ct sysvmsg sysvsem sysvshm \
 		tokenizer wddx xml xmlrpc yaz zip zlib
 # version specific components
 _USE_PHP_VER4=	${_USE_PHP_ALL} crack dbx dio domxml mcal mcve \
-		mnogosearch oracle overload xslt yp
+		mnogosearch oracle overload pfpro xslt yp
 _USE_PHP_VER5=	${_USE_PHP_ALL} dom mysqli simplexml soap sqlite \
 		tidy xmlreader xmlwriter xsl
 
