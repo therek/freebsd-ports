898d901694b3668aab67189695db0d4549b3da80
tmclaugh <tmclaugh@FreeBSD.org>
2006-02-19 06:14:39 +0000

    Update to 1.1.13.2
    Release notes:	http://go-mono.com/archive/1.1.13/
    
    Port changes:
    - Execute mono-semdel during pre-clean in addition to post-install
    - Execute mono-semdel over ${WRKSRC}/runtime as a precaution
    - Add G_DEBUG="" to mono-semdel target in bsd.mono.mk to prevent
      dropping core files with upcomming glib 2.9 [1]
    - add XSP_DOCROOT for when we eventually have an xsp port in the ports
      tree (If interested in xsp let me know)
    - Remove lingering USE_REINPLACE while I'm here
    
    Submitted by:	Andres Kohn [1]
    Project by:	BSD# <http://www.mono-project.com/Mono:FreeBSD>

--- lang/mono/bsd.mono.mk
+++ lang/mono/bsd.mono.mk
@@ -5,8 +5,8 @@
 # bsd.mono.mk: accomodate the peculiarities of building C# ports within
 # the FreeBSD ports system.
 #
-# $FreeBSD: ports/lang/mono/bsd.mono.mk,v 1.1 2005/11/10 04:17:25 tmclaugh Exp $
-#   $Id: bsd.mono.mk,v 1.7 2005/11/15 03:09:57 tmclau02 Exp $
+# $FreeBSD: ports/lang/mono/bsd.mono.mk,v 1.2 2006/01/14 21:34:06 tmclaugh Exp $
+#   $Id: bsd.mono.mk,v 1.10 2006/02/16 00:37:15 tmclau02 Exp $
 #
 
 # Set the location of the .wapi directory so we write to a location we
@@ -15,10 +15,12 @@ MONO_SHARED_DIR=${WRKDIR}
 CONFIGURE_ENV+=MONO_SHARED_DIR="${MONO_SHARED_DIR}"
 MAKE_ENV+=MONO_SHARED_DIR="${MONO_SHARED_DIR}"
 
+# Set the location that webaps served bp XSP should use.
+XSP_DOCROOT=${PREFIX}/www/xsp
 
 # Clean up the semaphore produced by the .wapi
 post-install: mono-semdel
 pre-clean: mono-semdel
 
 mono-semdel:
-	@${SETENV} MONO_SHARED_DIR="${MONO_SHARED_DIR}" ${LOCALBASE}/bin/mono-semdel 2> /dev/null || true
+	@${SETENV} G_DEBUG="" MONO_SHARED_DIR="${MONO_SHARED_DIR}" ${LOCALBASE}/bin/mono-semdel 2> /dev/null || true
