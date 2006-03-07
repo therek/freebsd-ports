9386ad3a440bedba83bf8ef7616e744ca318e830
knu <knu@FreeBSD.org>
2006-03-07 07:22:59 +0000

    Run ldconfig(8) as a pre-command for vjed, to get the compat3x libraries linked successfully.
    Since misc/compat3x is currently forbidden and the startup script is
    yet to be converted to use rc.subr, this is mandatory for vjed to be
    run properly on startup, because the vje.sh script uses rc.subr and
    seems to be invoked before 000.compat3x.sh.
    
    Approved by:	portmgr

--- japanese/vje30/files/vje.sh
+++ japanese/vje30/files/vje.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $FreeBSD: ports/japanese/vje30/files/vje.sh,v 1.2 2004/05/11 16:47:52 knu Exp $
+# $FreeBSD: ports/japanese/vje30/files/vje.sh,v 1.3 2006/02/20 20:47:06 dougb Exp $
 
 # PROVIDE: vjed
 # REQUIRE: DAEMON
@@ -12,9 +12,9 @@
 name=vjed
 rcvar=`set_rcvar`
 
-command=%%PREFIX%%/vje30/bin/vjed
-
 required_files=%%PREFIX%%/etc/vje30/vje.cfg
+start_precmd='/sbin/ldconfig -m %%PREFIX%%/lib/compat'
+command=%%PREFIX%%/vje30/bin/vjed
 
 [ -z "$vjed_enable" ] && vjed_enable=NO
 
