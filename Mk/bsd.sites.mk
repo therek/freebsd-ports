fc04f0d7ff70bc6fbc7f651fd3219ee062e27983
garga <garga@FreeBSD.org>
2006-03-21 22:22:43 +0000

    back out last commit, MASTER_SORT can do the job and much more

--- Mk/bsd.sites.mk
+++ Mk/bsd.sites.mk
@@ -20,7 +20,7 @@
 #
 # Note: all entries should terminate with a slash.
 #
-# $FreeBSD: ports/Mk/bsd.sites.mk,v 1.356 2006/03/21 15:40:26 garga Exp $
+# $FreeBSD: ports/Mk/bsd.sites.mk,v 1.357 2006/03/21 15:53:42 garga Exp $
 #
 
 # Where to put distfiles that don't have any other master site
@@ -947,7 +947,7 @@ MASTER_SITE_SAVANNAH+= \
 .endif
 
 .if !defined(IGNORE_MASTER_SITE_SOURCEFORGE)
-.for mirror in ${MASTER_SITE_SOURCEFORGE_PREFERRED} heanet nchc optusnet kent ufpr surfnet jaist umn osdn
+.for mirror in heanet nchc optusnet kent ufpr surfnet jaist umn osdn
 MASTER_SITE_SOURCEFORGE+= \
 	http://${mirror}.dl.sourceforge.net/sourceforge/%SUBDIR%/
 .endfor
