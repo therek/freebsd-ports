e3496ca111c07e4426ca2729b1733e94941bbb24
linimon <linimon@FreeBSD.org>
2006-07-05 19:53:27 +0000

    Whitespace cleanup.

--- Mk/bsd.scons.mk
+++ Mk/bsd.scons.mk
@@ -1,7 +1,7 @@
 #-*- mode: Makefile; tab-width: 4; -*-
 # ex:ts=4
 #
-# $FreeBSD: ports/Mk/bsd.scons.mk,v 1.1 2006/07/05 02:11:20 linimon Exp $
+# $FreeBSD: ports/Mk/bsd.scons.mk,v 1.2 2006/07/05 19:50:28 linimon Exp $
 #
 # bsd.scons.mk - Python-based SCons build system interface.
 # Author: Alexander Botero-Lowry <alex@foxybanana.com>
@@ -20,14 +20,14 @@ SCONS_MAINTAINER=	alex@foxybanana.com
 SCONS_BIN=	${LOCALBASE}/bin/scons
 SCONS_PORT=	${PORTSDIR}/devel/scons
 
-# 
+#
 # CCFLAGS is the scons equivalent of CFLAGS. So we should bring in our
 # FreeBSD CFLAGS.
 #
 # LINKFLAGS is equivalent to LDFLAGS in make speak, so we bring in the
 # FreeBSD default LDFLAGS.
 #
-# Some scons projects may honor PKGCONFIGDIR, which tells them where to 
+# Some scons projects may honor PKGCONFIGDIR, which tells them where to
 # look for, and install, pkgconfig files.
 #
 CCFLAGS?=	${CFLAGS}
@@ -38,15 +38,15 @@ CCFLAGS+=	-I${LOCALBASE}/include
 CXXFLAGS+=	-I${LOCALBASE}/include
 LINKFLAGS+=	-L${LOCALBASE}/lib
 
-# 
+#
 # SCONS_ENV is where we pass all the stuff that should be the
 # same for any scons port to scons. Things like CCFLAGS, and LINKFLAGS
 # go here.
 #
-# SCONS_ARG is where you pass port specific scons flags to the scons 
+# SCONS_ARG is where you pass port specific scons flags to the scons
 # enviornment.
 #
-# SCONS_BUILDENV is where you pass variables you want to be in the 
+# SCONS_BUILDENV is where you pass variables you want to be in the
 # System Enviornment instead of the SCons Enviornment.
 #
 # SCONS_TARGET is the same as MAKE_TARGET it is passed as the last
@@ -55,20 +55,20 @@ LINKFLAGS+=	-L${LOCALBASE}/lib
 SCONS_ENV?=	CCFLAGS="${CCFLAGS}" CXXFLAGS="${CXXFLAGS}" \
 		LINKFLAGS="${LINKFLAGS}" PKGCONFIGDIR="${PKGCONFIGDIR}" \
 		PREFIX="${PREFIX}" CC="${CC}" CXX="${CXX}"
-SCONS_ARGS?=	
-SCONS_BUILDENV?=	
+SCONS_ARGS?=
+SCONS_BUILDENV?=
 SCONS_TARGET?=
 
 #
-# SCONS_INSTALL_TARGET is the default target to be used when 
+# SCONS_INSTALL_TARGET is the default target to be used when
 # installing a port using scons.
 #
 SCONS_INSTALL_TARGET?=	${INSTALL_TARGET}
 
-# 
+#
 # Make sure we depend on scons
 #
-BUILD_DEPENDS+= ${SCONS_BIN}:${SCONS_PORT}
+BUILD_DEPENDS+=	${SCONS_BIN}:${SCONS_PORT}
 
 .if !target(do-build)
 do-build:
