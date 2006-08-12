1f3ca193b90eb9a7c960fa0c8523db474204501b
dinoex <dinoex@FreeBSD.org>
2006-08-12 20:10:49 +0000

    - improve error handling Submitted by:	Royce Williams

--- mail/sendmail/files/sendmail.sh
+++ mail/sendmail/files/sendmail.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $FreeBSD: ports/mail/sendmail/files/sendmail.sh,v 1.2 2002/04/08 02:48:26 dinoex Exp $
+# $FreeBSD: ports/mail/sendmail/files/sendmail.sh,v 1.3 2003/12/04 03:24:09 dinoex Exp $
 # 
 if ! test -x %%PREFIX%%/sbin/sendmail
 then
@@ -34,21 +34,29 @@ stop)
 	then
 		kill `head -1 /var/run/sendmail.pid`
 		rm -f /var/run/sendmail.pid
+	else
+		echo "ERROR: pidfile /var/run/sendmail.pid does not exist."
 	fi
 	if test -e /var/spool/clientmqueue/sm-client.pid
 	then
 		kill `head -1 /var/spool/clientmqueue/sm-client.pid`
 		rm -f /var/spool/clientmqueue/sm-client.pid
+	else
+		echo "ERROR: pidfile /var/spool/clientmqueue/sm-client.pid does not exist."
 	fi
 	;;
 restart)
 	if test -e /var/run/sendmail.pid
 	then
 		kill -1 `head -1 /var/run/sendmail.pid`
+	else
+		echo "ERROR: pidfile /var/run/sendmail.pid does not exist."
 	fi
 	if test -e /var/spool/clientmqueue/sm-client.pid
 	then
 		kill -1 `head -1 /var/spool/clientmqueue/sm-client.pid`
+	else
+		echo "ERROR: pidfile /var/spool/clientmqueue/sm-client.pid does not exist."
 	fi
 	;;
 *)
