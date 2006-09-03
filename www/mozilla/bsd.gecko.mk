8efc728ac0b5e9ab03b3dfbf76e71bfaeb1fa579
ahze <ahze@FreeBSD.org>
2006-09-03 20:22:15 +0000

    - Remove firefox10

--- www/mozilla/bsd.gecko.mk
+++ www/mozilla/bsd.gecko.mk
@@ -1,7 +1,7 @@
 #-*- mode: Fundamental; tab-width: 4; -*-
 # ex:ts=4
 #
-# $FreeBSD: ports/www/mozilla/bsd.gecko.mk,v 1.14 2006/06/21 01:17:35 ahze Exp $
+# $FreeBSD: ports/www/mozilla/bsd.gecko.mk,v 1.15 2006/07/05 22:12:13 ahze Exp $
 #    $MCom: ports/www/mozilla/bsd.gecko.mk,v 1.17 2006/03/10 12:42:50 ahze Exp $
 #
 # 4 column tabs prevent hair loss and tooth decay!
@@ -71,7 +71,6 @@ Gecko_Pre_Include=			bsd.gecko.mk
 .if (${ARCH}!="sparc64" || ${OSVERSION} >= 601101) && ${ARCH}!="ia64"
 _GECKO_ALL=	firefox nvu seamonkey sunbird thunderbird xulrunner firefox-devel flock
 .endif
-_GECKO_ALL+=	firefox10
 .endif
 _GECKO_ALL+=	mozilla
 
