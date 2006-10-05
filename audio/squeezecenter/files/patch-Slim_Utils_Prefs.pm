002a9bfa7aae23427c63c6f27956a6f32d56c457
brooks <brooks@FreeBSD.org>
2006-10-05 02:56:56 +0000

    Upgrade slimserver to version 6.5.0.
    The biggest change is that the database is now MySQL instead of SQLite.
    Slimserver starts it's own MySQL instance so you don't need to deal with
    MySQL configuration, but both the client and server are now
    dependencies.  For more info see the change log at:
    
    	http://www.slimdevices.com/Changelog6.html
    
    Most of the binary perl modules are now port dependencies.  YAML-Syck is
    not yet because it didn't work.  I suspect a version issues (the ports
    version is newer).

--- audio/squeezecenter/files/patch-Slim_Utils_Prefs.pm
+++ audio/squeezecenter/files/patch-Slim_Utils_Prefs.pm
@@ -1,9 +1,9 @@
 
-$FreeBSD: ports/audio/slimserver/files/patch-Slim_Utils_Prefs.pm,v 1.3 2005/04/21 05:17:02 brooks Exp $
+$FreeBSD$
 
 --- Slim/Utils/Prefs.pm.orig
 +++ Slim/Utils/Prefs.pm
-@@ -99,6 +99,10 @@
+@@ -392,6 +392,10 @@
  
  		$path = $Bin . '/Playlists';
  
@@ -14,16 +14,16 @@ $FreeBSD: ports/audio/slimserver/files/patch-Slim_Utils_Prefs.pm,v 1.3 2005/04/2
  	} else {
  
  		$path = '';
-@@ -131,7 +135,7 @@
+@@ -424,7 +428,7 @@
  
  	} elsif ($os eq 'unix') {
  
--		$CacheDir = $ENV{'HOME'};
+-		$CacheDir = catdir($ENV{'HOME'},'Cache');
 +		$CacheDir = "/var/db/slimserver/cache";
  	}
  
  	my @CacheDirs = splitdir($CacheDir);
-@@ -714,7 +718,7 @@
+@@ -1014,7 +1018,7 @@
  	} elsif (Slim::Utils::OSDetect::OS() eq 'win')  {
  		$prefsPath = $Bin;
  	} else {
