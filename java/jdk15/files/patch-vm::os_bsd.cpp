e783fc10d803a7f07835bf0af07520a0452abf0a
glewis <glewis@FreeBSD.org>
2006-11-06 19:54:00 +0000

    . Fix a problem with os_sleep returning early without raising an exception   when its called with interruptible set to false.
    Submitted by:	Kurt Miller <kurt@intricatesoftware.com>

--- java/jdk15/files/patch-vm::os_bsd.cpp
+++ java/jdk15/files/patch-vm::os_bsd.cpp
@@ -1,4 +1,4 @@
-$FreeBSD$
+$FreeBSD: ports/java/jdk15/files/patch-vm::os_bsd.cpp,v 1.4 2006/10/14 08:54:34 marcus Exp $
 
 --- ../../hotspot/src/os/bsd/vm/os_bsd.cpp.orig	Tue Jan 27 17:54:28 2004
 +++ ../../hotspot/src/os/bsd/vm/os_bsd.cpp	Tue Jan 27 17:55:21 2004
@@ -11,3 +11,35 @@ $FreeBSD$
  #define EXTENSIONS_DIR "/lib/ext"
  #define ENDORSED_DIR "/lib/endorsed"
  
+@@ -1968,16 +1968,23 @@
+ 
+ static int os_sleep(jlong millis, bool interruptible) {
+   int res;
+-  if (!interruptible) {
+-    do {
+-	res = os_nanosleep(&millis);
+-    } while ((res == OS_ERR) && (errno == EINTR) && (millis > 0));
+-  } else {
+-    INTERRUPTIBLE_NORESTART_VM(os_nanosleep(&millis), res, os::Bsd::clear_interrupted);
++
++  while (millis > 0) {
++    if (!interruptible) {
++      res = os_nanosleep(&millis);
++    } else {
++      INTERRUPTIBLE_NORESTART_VM(os_nanosleep(&millis), res, os::Bsd::clear_interrupted);
++    }
++    // INTERRUPTIBLE_NORESTART_VM returns res == OS_INTRPT for thread.Interrupt
++
++    if((res == OS_ERR) && (errno == EINTR)) {
++      if(millis <= 0)
++	return OS_OK;
++    } else
++      return res;
+   }
+-  // INTERRUPTIBLE_NORESTART_VM returns res == OS_INTRPT for thread.Interrupt
+ 
+-  return res;
++  return OS_OK;
+ }
+ 
+ int os::Bsd::naked_sleep() {
