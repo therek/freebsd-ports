43d0b30b56ba4148d0bcac5c4fc2bece1cc9b5ef
ache <ache@FreeBSD.org>
2006-12-10 05:10:38 +0000

    Upgrade to 3.0

--- mail/milter-greylist/files/milter-greylist.sh
+++ mail/milter-greylist/files/milter-greylist.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $FreeBSD: ports/mail/milter-greylist/files/milter-greylist.sh,v 1.2 2004/07/29 20:07:03 pav Exp $
+# $FreeBSD: ports/mail/milter-greylist/files/milter-greylist.sh,v 1.3 2005/06/30 11:55:19 flz Exp $
 
 # PROVIDE: miltergreylist
 # REQUIRE: LOGIN
@@ -22,13 +22,13 @@ rcvar=`set_rcvar`
 load_rc_config $name
 
 miltergreylist_enable=${miltergreylist_enable-"NO"}
-miltergreylist_runas=${miltergreylist_runas-"smmsp"}
+miltergreylist_runas=${miltergreylist_runas-"mailnull"}
 miltergreylist_pidfile=${miltergreylist_pidfile-"/var/run/milter-greylist.pid"}
 miltergreylist_sockfile=${miltergreylist_sockfile-"/var/milter-greylist/milter-greylist.sock"}
 miltergreylist_cfgfile=${miltergreylist_cfgfile-"%%PREFIX%%/etc/mail/greylist.conf"}
 miltergreylist_flags=${miltergreylist_flags-"-P $miltergreylist_pidfile \
 -f $miltergreylist_cfgfile -p $miltergreylist_sockfile -u $miltergreylist_runas"}
 
-command="%%PREFIX%%/bin/milter-greylist"
+command="%%PREFIX%%/libexec/milter-greylist"
 
 run_rc_command "$1"
