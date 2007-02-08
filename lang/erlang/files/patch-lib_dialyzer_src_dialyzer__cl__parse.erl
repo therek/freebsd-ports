aeee40846354eb0b81355ce0db34725b6ec6af8a
olgeni <olgeni@FreeBSD.org>
2007-02-08 22:41:36 +0000

    Fix dialyzer_init_plt path.

--- lang/erlang/files/patch-lib_dialyzer_src_dialyzer__cl__parse.erl
+++ lang/erlang/files/patch-lib_dialyzer_src_dialyzer__cl__parse.erl
@@ -1,5 +1,5 @@
 
-$FreeBSD$
+$FreeBSD: ports/lang/erlang/files/patch-lib_dialyzer_src_dialyzer__cl__parse.erl,v 1.2 2007/02/02 13:06:22 olgeni Exp $
 
 --- lib/dialyzer/src/dialyzer_cl_parse.erl.orig
 +++ lib/dialyzer/src/dialyzer_cl_parse.erl
@@ -8,7 +8,7 @@ $FreeBSD$
  
  init() ->
 -  InitPlt = filename:join([code:lib_dir(dialyzer), "plt","dialyzer_init_plt"]),
-+  InitPlt = filename:join([os:getenv ("HOME"), "plt","dialyzer_init_plt"]),
++  InitPlt = filename:join([os:getenv ("HOME"), ".dialyzer_init_plt"]),
    put(dialyzer_init_plt, InitPlt),
    put(dialyzer_only_check_init_plt, false),
    put(dialyzer_options_gui, true),
