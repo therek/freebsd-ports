4f615053e5bc796516dae446d0173a64079ea89a
pav <pav@FreeBSD.org>
2007-04-02 22:39:20 +0000

    - Remove now unused tcl81 category, add new categories tcl and tk
    Tested on:	pointyhat exp-run

--- Mk/bsd.port.mk
+++ Mk/bsd.port.mk
@@ -1,7 +1,7 @@
 #-*- mode: makefile; tab-width: 4; -*-
 # ex:ts=4
 #
-# $FreeBSD: ports/Mk/bsd.port.mk,v 1.556 2007/03/14 04:06:55 linimon Exp $
+# $FreeBSD: ports/Mk/bsd.port.mk,v 1.557 2007/03/24 14:02:06 pav Exp $
 #	$NetBSD: $
 #
 #	bsd.port.mk - 940820 Jordan K. Hubbard.
@@ -2766,8 +2766,8 @@ VALID_CATEGORIES+= accessibility afterstep arabic archivers astro audio \
 	palm parallel pear perl5 plan9 polish portuguese ports-mgmt \
 	print python ruby rubygems russian \
 	scheme science security shells spanish sysutils \
-	tcl80 tcl81 tcl82 tcl83 tcl84 textproc \
-	tk80 tk82 tk83 tk84 tkstep80 \
+	tcl tcl80 tcl82 tcl83 tcl84 textproc \
+	tk tk80 tk82 tk83 tk84 tkstep80 \
 	ukrainian vietnamese windowmaker www \
 	x11 x11-clocks x11-fm x11-fonts x11-servers x11-themes x11-toolkits \
 	x11-wm xfce zope
