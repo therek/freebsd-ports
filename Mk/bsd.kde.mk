2e4cf9c1f0082e982e0675ad8719e62c45e2aeb9
lofi <lofi@FreeBSD.org>
2007-07-04 10:25:00 +0000

    Update to KDE 3.5.7 / KOffice 1.6.3

--- Mk/bsd.kde.mk
+++ Mk/bsd.kde.mk
@@ -1,7 +1,7 @@
 #-*- mode: Makefile; tab-width: 4; -*-
 # ex:ts=4
 #
-# $FreeBSD: ports/Mk/bsd.kde.mk,v 1.71 2007/04/12 12:33:34 lofi Exp $
+# $FreeBSD: ports/Mk/bsd.kde.mk,v 1.72 2007/04/19 17:07:59 lofi Exp $
 #
 # Please view me with 4 column tabs!
 
@@ -104,7 +104,7 @@ CONFIGURE_ENV+=	MOC="${MOC}" CPPFLAGS="${CPPFLAGS} ${QTCPPFLAGS}" LIBS="${QTCFGL
 
 # Yeah, it's namespace pollution, but this is really the best place for this
 # stuff. Arts does NOT use it anymore.
-KDE_VERSION=		3.5.6
+KDE_VERSION=		3.5.7
 KDE_ORIGVER=	${KDE_VERSION}
 KDE_PREFIX?=	${LOCALBASE}
 
