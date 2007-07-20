7090945772f6a75e4456d911f87e261ff36dcd1d
gabor <gabor@FreeBSD.org>
2007-07-20 20:44:10 +0000

    - Update to 1.1.8 - Unbreak on 7.X
    PR:		ports/114693
    Submitted by:	Janos Mohacsi <janos.mohacsi@bsd.hu>

--- hungarian/hunspell/files/patch-src_tools_hunspell.cxx
+++ hungarian/hunspell/files/patch-src_tools_hunspell.cxx
@@ -1,14 +1,27 @@
 
-$FreeBSD$
+$FreeBSD: ports/hungarian/hunspell/files/patch-src_tools_hunspell.cxx,v 1.1 2006/11/10 14:11:20 miwi Exp $
 
 --- src/tools/hunspell.cxx.orig
 +++ src/tools/hunspell.cxx
-@@ -47,7 +47,7 @@
+@@ -59,16 +59,12 @@
  #include "manparser.hxx"
  #include "firstparser.hxx"
  
--#define LIBDIR "/usr/share/myspell/"
-+#define LIBDIR "%%PREFIX%%/share/myspell/"
+-#define LIBDIR "/usr/share/hunspell:/usr/share/myspell"
++#define LIBDIR "%%PREFIX%%/share/hunspell:%%PREFIX%%/share/myspell"
+ #define USEROOODIR ".openoffice.org2/user/wordbook"
+-#define OOODIR "/opt/openoffice.org2.3/share/dict/ooo:" \
+-    "/usr/lib/openoffice.org2.3/share/dict/ooo:" \
+-    "/opt/openoffice.org2.2/share/dict/ooo:" \
+-    "/usr/lib/openoffice.org2.2/share/dict/ooo:" \
+-    "/opt/openoffice.org2.1/share/dict/ooo:" \
+-    "/usr/lib/openoffice.org2.1/share/dict/ooo:" \
+-    "/opt/openoffice.org2.0/share/dict/ooo:" \
+-    "/usr/lib/openoffice.org2.0/share/dict/ooo"
++#define OOODIR "%%PREFIX%%/openoffice.org-2.3/share/dict/ooo:" \
++    "%%PREFIX%%/openoffice.org-2.2/share/dict/ooo:" \
++    "%%PREFIX%%/openoffice.org-2.1/share/dict/ooo:" \
++    "%%PREFIX%%/openoffice.org2.0/share/dict/ooo"
  #define HOME getenv("HOME")
  #define DICBASENAME ".hunspell_"
  #define LOGFILE "/tmp/hunspell.log"
