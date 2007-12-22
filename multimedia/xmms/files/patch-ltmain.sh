8604421fc5cde6442fd22cc2fbf7853f5f34dafe
stefan <stefan@FreeBSD.org>
2007-12-22 13:48:45 +0000

    Update to 1.2.11.
    PR:		118336
    Submitted by:	Ulrich Spoerlein <uspoerlein@gmail.com>
    Approved by:	maintainer timeout (>3 weeks)

--- multimedia/xmms/files/patch-ltmain.sh
+++ multimedia/xmms/files/patch-ltmain.sh
@@ -1,9 +1,6 @@
-
-$FreeBSD: ports/multimedia/xmms/files/patch-ltmain.sh,v 1.2 2002/03/05 21:09:09 pat Exp $
-
 --- ltmain.sh.orig	Fri Sep  5 13:18:43 2003
 +++ ltmain.sh	Fri Sep  5 13:18:09 2003
-@@ -4333,10 +4333,12 @@
+@@ -6038,10 +6038,12 @@
  	fi
  
  	# Install the pseudo-library for information purposes.
