dbd5f61063ea55de59d7c2ebf400c94e89f5b608
miwi <miwi@FreeBSD.org>
2008-01-07 23:13:38 +0000

    - Update to 2.1
    Submitted by:	farrokhi@ (maintainer)

--- www/eventum/files/patch-misc_monitor.php
+++ www/eventum/files/patch-misc_monitor.php
@@ -1,5 +1,5 @@
 
-$FreeBSD$
+$FreeBSD: ports/www/eventum/files/patch-misc_monitor.php,v 1.1 2007/08/05 08:37:36 miwi Exp $
 
 --- misc/monitor.php.orig
 +++ misc/monitor.php
@@ -8,20 +8,20 @@ $FreeBSD$
      APP_PATH . 'config/config.php' => array(
          'check_owner'      => true,
 -        'owner'            => 'apache',
-+        'owner'            => 'www',
++        'owner'            => '%%WWWOWN%%',
          'check_group'      => true,
 -        'group'            => 'apache',
-+        'group'            => 'www',
++        'group'            => '%%WWWGRP%%',
          'check_permission' => true,
          'permission'       => 755,
      ),
      APP_PATH . 'config/setup.php' => array(
          'check_owner'      => true,
 -        'owner'            => 'apache',
-+        'owner'            => 'www',
++        'owner'            => '%%WWWOWN%%',
          'check_group'      => true,
 -        'group'            => 'apache',
-+        'group'            => 'www',
++        'group'            => '%%WWWGRP%%',
          'check_permission' => true,
          'permission'       => 750,
          'check_filesize'   => true,
