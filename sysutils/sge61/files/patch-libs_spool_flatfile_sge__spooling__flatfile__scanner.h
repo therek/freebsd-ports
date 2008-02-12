81a65570b924d7fe16a3f64b7a43405fa3cb5624
brooks <brooks@FreeBSD.org>
2008-02-12 20:55:46 +0000

    Note repocopy of sysutils/sge to sysutils/sge60.
    Repocopy by:	marcus
    PR:		ports/120121

--- sysutils/sge61/files/patch-libs_spool_flatfile_sge__spooling__flatfile__scanner.h
+++ sysutils/sge61/files/patch-libs_spool_flatfile_sge__spooling__flatfile__scanner.h
@@ -1,5 +1,5 @@
 
-$FreeBSD$
+$FreeBSD: ports/sysutils/sge61/files/patch-libs_spool_flatfile_sge__spooling__flatfile__scanner.h,v 1.2 2007/02/05 17:33:22 brooks Exp $
 
 --- libs/spool/flatfile/sge_spooling_flatfile_scanner.h.orig
 +++ libs/spool/flatfile/sge_spooling_flatfile_scanner.h
