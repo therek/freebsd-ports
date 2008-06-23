8177e4681c880660dbce078023e2daf836e40c28
tdb <tdb@FreeBSD.org>
2008-06-23 12:18:35 +0000

    mirror.ac.uk closed down during 2007:
    	"JISC ceased funding for the JISC Mirror service during 2007.
    	If you came to this page via a referring site, you may wish to
    	let that site know that its links are out of date."

--- Mk/bsd.sites.mk
+++ Mk/bsd.sites.mk
@@ -20,7 +20,7 @@
 #
 # Note: all entries should terminate with a slash.
 #
-# $FreeBSD: ports/Mk/bsd.sites.mk,v 1.451 2008/06/02 07:42:56 itetcu Exp $
+# $FreeBSD: ports/Mk/bsd.sites.mk,v 1.452 2008/06/18 19:47:22 pav Exp $
 #
 
 # Where to put distfiles that don't have any other master site
@@ -1306,8 +1306,6 @@ MASTER_SITE_SUSE+= 	\
 	http://ftp.isu.edu.tw/pub/Linux/SuSE/%SUBDIR%/ \
 	ftp://ftp.mirrorservice.org/sites/ftp.suse.com/pub/suse/%SUBDIR%/ \
 	http://www.mirrorservice.org/sites/ftp.suse.com/pub/suse/%SUBDIR%/ \
-	ftp://ftp.mirror.ac.uk/sites/ftp.suse.com/%SUBDIR%/ \
-	http://download.mirror.ac.uk/sites/ftp.suse.com/%SUBDIR%/ \
 	ftp://ftp.kde.org/pub/suse/ftp.suse.com/suse/%SUBDIR%/ \
 	ftp://ftp.suse.com/pub/suse/%SUBDIR%/ \
 	ftp://ftp.softnet.tuc.gr/pub/linux/suse/suse/%SUBDIR%/ \
