115746944b1a07abcca094606d012f6f1ea16381
marcus <marcus@FreeBSD.org>
2008-09-08 03:04:31 +0000

    Update to 2.20.8.

--- x11/gdm/files/patch-gui_gdmchooser.c
+++ x11/gdm/files/patch-gui_gdmchooser.c
@@ -1,17 +1,8 @@
 
-$FreeBSD$
+$FreeBSD: ports/x11/gdm/files/patch-gui_gdmchooser.c,v 1.3 2008/07/26 01:20:04 vs Exp $
 
 --- gui/gdmchooser.c.orig
 +++ gui/gdmchooser.c
-@@ -727,7 +727,7 @@
- 			/* paranoia */
- 			ifreq.ifr_name[sizeof (ifreq.ifr_name) - 1] = '\0';
- 
--			if (ioctl (sock, SIOCGIFFLAGS, &ifreq) < 0) 
-+			if ((ioctl (sock, SIOCGIFFLAGS, &ifreq) < 0) && (errno != ENXIO))
- 				gdm_common_error ("Could not get SIOCGIFFLAGS for %s", ifr[i].ifr_name);
- 
- 			if ((ifreq.ifr_flags & IFF_UP) == 0 ||
 @@ -1123,6 +1123,12 @@
  		have_ipv6 = FALSE;
  	else
