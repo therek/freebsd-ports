c1139402911d723682c5cddce1a9f8bc94429e7d
sobomax <sobomax@FreeBSD.org>
2009-01-13 02:00:29 +0000

    Fix on FreeBSD 7 and up. Define uint using typedef directive, not #define.

--- net/asterisk12/files/patch-include_asterisk_astobj2.h
+++ net/asterisk12/files/patch-include_asterisk_astobj2.h
@@ -1,5 +1,5 @@
 
-$FreeBSD$
+$FreeBSD: ports/net/asterisk12/files/patch-include_asterisk_astobj2.h,v 1.1 2009/01/12 20:48:54 sobomax Exp $
 
 --- include/asterisk/astobj2.h
 +++ include/asterisk/astobj2.h
@@ -7,7 +7,7 @@ $FreeBSD$
  #ifndef _ASTERISK_ASTOBJ2_H
  #define _ASTERISK_ASTOBJ2_H
  
-+#define uint unsigned int
++typedef unsigned int    uint;
 +
  #include "asterisk/compat.h"
  
