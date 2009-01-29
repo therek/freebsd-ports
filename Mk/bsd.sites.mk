a020a385213417d745de4cc01382f72225fcce95
pav <pav@FreeBSD.org>
2009-01-29 13:53:29 +0000

    - PHP: push brazilian and chinese mirrors to the end of the list, so well   connected danmark mirror is tried first.  Sorting mirror sites alphabetically   is not a good idea.

--- Mk/bsd.sites.mk
+++ Mk/bsd.sites.mk
@@ -20,7 +20,7 @@
 #
 # Note: all entries should terminate with a slash.
 #
-# $FreeBSD: ports/Mk/bsd.sites.mk,v 1.467 2009/01/02 12:36:17 pav Exp $
+# $FreeBSD: ports/Mk/bsd.sites.mk,v 1.468 2009/01/29 13:48:52 pav Exp $
 #
 
 # Where to put distfiles that don't have any other master site
@@ -994,8 +994,6 @@ MASTER_SITE_PGSQL+= \
 
 .if !defined(IGNORE_MASTER_SITE_PHP)
 MASTER_SITE_PHP+= \
-	http://br.php.net/%SUBDIR%/ \
-	http://cn.php.net/%SUBDIR%/ \
 	http://dk.php.net/%SUBDIR%/ \
 	http://de.php.net/%SUBDIR%/ \
 	http://es.php.net/%SUBDIR%/ \
@@ -1007,6 +1005,8 @@ MASTER_SITE_PHP+= \
 	http://se.php.net/%SUBDIR%/ \
 	http://uk.php.net/%SUBDIR%/ \
 	http://us2.php.net/%SUBDIR%/ \
+	http://br.php.net/%SUBDIR%/ \
+	http://cn.php.net/%SUBDIR%/ \
 	${MASTER_SITE_RINGSERVER:S,%SUBDIR%,net/www/php/&,}
 .endif
 
