4d94fb58faa374ed36c06bc38caccb9086e69456
pav <pav@FreeBSD.org>
2009-05-11 17:35:18 +0000

    - Add a code to drop bsd.port.options.mk into /usr/share/mk if it's missing on   the system (typically FreeBSD prior to 6.3).  Allows to continue if the write   fails.  This should allow us to start using it widely in the ports tree   (please not just yet, wait for a heads-up, in case this change needs to be   backed out.)

--- Mk/bsd.port.mk
+++ Mk/bsd.port.mk
@@ -1,7 +1,7 @@
 #-*- mode: makefile; tab-width: 4; -*-
 # ex:ts=4
 #
-# $FreeBSD: ports/Mk/bsd.port.mk,v 1.616 2009/03/28 20:45:06 skv Exp $
+# $FreeBSD: ports/Mk/bsd.port.mk,v 1.617 2009/04/09 22:40:20 pav Exp $
 #	$NetBSD: $
 #
 #	bsd.port.mk - 940820 Jordan K. Hubbard.
@@ -2816,6 +2816,19 @@ maintainer:
 
 .if !target(check-makefile)
 check-makefile::
+.if !exists(/usr/share/mk/bsd.port.options.mk)
+	@${ECHO_CMD} "!!! Detected system without bsd.port.options.mk (probably old FreeBSD version)"
+	@${ECHO_CMD} "!!! Dropping bsd.port.options.mk into /usr/share/mk"
+	-@${ECHO_CMD} "USEOPTIONSMK=   yes" > /usr/share/mk/bsd.port.options.mk 2>/dev/null
+	-@${ECHO_CMD} "INOPTIONSMK=    yes" >> /usr/share/mk/bsd.port.options.mk 2>/dev/null
+	-@${ECHO_CMD} ".include <bsd.port.mk>" >> /usr/share/mk/bsd.port.options.mk 2>/dev/null
+	-@${ECHO_CMD} ".undef INOPTIONSMK" >> /usr/share/mk/bsd.port.options.mk 2>/dev/null
+.if exists(/usr/share/mk/bsd.port.options.mk)
+	@${ECHO_CMD} "!!! Done"
+.else
+	@${ECHO_CMD} "!!! Failed"
+.endif
+.endif
 	@${DO_NADA}
 .endif
 
