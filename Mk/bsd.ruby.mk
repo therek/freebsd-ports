b95be0304df35255b893a0628129123bddaa4126
stas <stas@FreeBSD.org>
2009-06-02 17:25:37 +0000

    - Update ruby 1.8.7 to the latest revision available from ruby-lang.org.   This updates also borrows some important bugfixes from Ruby CVS that   is not available in patchlevel 160 yet. - Drop GC patch support. It is broken with the new ruby version and   seem to require the complete rewriting.

--- Mk/bsd.ruby.mk
+++ Mk/bsd.ruby.mk
@@ -3,7 +3,7 @@
 #
 # Created by: Akinori MUSHA <knu@FreeBSD.org>
 #
-# $FreeBSD: ports/Mk/bsd.ruby.mk,v 1.178 2009/02/13 23:31:32 stas Exp $
+# $FreeBSD: ports/Mk/bsd.ruby.mk,v 1.179 2009/02/23 00:44:22 stas Exp $
 #
 
 .if !defined(Ruby_Include)
@@ -170,9 +170,9 @@ RUBY?=			${LOCALBASE}/bin/${RUBY_NAME}
 # Ruby 1.8
 #
 RUBY_RELVERSION=	1.8.7
-RUBY_PORTREVISION=	1
+RUBY_PORTREVISION=	0
 RUBY_PORTEPOCH=		1
-RUBY_PATCHLEVEL=	72
+RUBY_PATCHLEVEL=	160
 
 .  if ${RUBY_PATCHLEVEL} == 0
 RUBY_VERSION?=		${RUBY_RELVERSION}
