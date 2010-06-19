69004c24a60e267edad53312416529a1c4caecb8
fluffy <fluffy@FreeBSD.org>
2010-06-19 02:12:56 +0000

    - Update KOffice (and its l10n packs) to 2.2.0 release
    Major changes:
        Kexi now returns back
        New import filters for MS OOXML (Office 2007) Formats
        Improved OO.org and MS Office traditional formats compatibility
        Improved stability at all
    
    Discussed with:	miwi, itetcu
    Approved by:	portmgr (erwin)
    Feature safe:	yes

--- Mk/bsd.kde4.mk
+++ Mk/bsd.kde4.mk
@@ -1,7 +1,7 @@
 #-*- mode: Makefile; tab-width: 4; -*-
 # ex:ts=4
 #
-# $FreeBSD: ports/Mk/bsd.kde4.mk,v 1.94 2010/05/14 05:21:56 fluffy Exp $
+# $FreeBSD: ports/Mk/bsd.kde4.mk,v 1.95 2010/06/01 21:35:18 makc Exp $
 #
 
 .if !defined(_POSTMKINCLUDED) && !defined(Kde_Pre_Include)
@@ -54,7 +54,7 @@ Kde_Include_MAINTAINER=	kde@FreeBSD.org
 
 KDE4_VERSION=		4.4.4
 KDE4_BRANCH?=		stable
-KOFFICE2_VERSION=	2.1.2
+KOFFICE2_VERSION=	2.2.0
 KOFFICE2_BRANCH?=	stable
 KDEVELOP_VERSION=	4.0.0
 KDEVELOP_BRANCH?=	stable
