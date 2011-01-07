69784e78fbd44111cb66ec5b8582a699ec1bb31a
fluffy <fluffy@FreeBSD.org>
2011-01-07 23:54:08 +0000

    - The KDE FreeBSD team is proud to announce the release of KDE 4.5.5 for FreeBSD.
    It's a bugfix-only release, January 2011 set.

--- Mk/bsd.kde4.mk
+++ Mk/bsd.kde4.mk
@@ -1,7 +1,7 @@
 #-*- mode: Makefile; tab-width: 4; -*-
 # ex:ts=4
 #
-# $FreeBSD: ports/Mk/bsd.kde4.mk,v 1.107 2010/12/04 19:55:14 makc Exp $
+# $FreeBSD: ports/Mk/bsd.kde4.mk,v 1.108 2010/12/24 22:44:52 makc Exp $
 #
 
 .if !defined(_POSTMKINCLUDED) && !defined(Kde_Pre_Include)
@@ -52,7 +52,7 @@ Kde_Include_MAINTAINER=	kde@FreeBSD.org
 #		but this could be changed in a future.
 #
 
-KDE4_VERSION=		4.5.4
+KDE4_VERSION=		4.5.5
 KDE4_BRANCH?=		stable
 KDEPIM4_VERSION=	4.4.9
 KDEPIM4_BRANCH?=	stable
