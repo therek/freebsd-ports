ed555dfb0589ce50c03768ead155c91f385d4c3c
pav <pav@FreeBSD.org>
2011-01-25 13:39:44 +0000

    - Propagate return code of the DESTDIR chrooted make
    PR:		ports/153597
    Submitted by:	HIROSHI OOTA <xalaxy@gmail.com>
    Feature safe:	yes

--- Mk/bsd.destdir.mk
+++ Mk/bsd.destdir.mk
@@ -5,7 +5,7 @@
 #
 # Created by: Gabor Kovesdan <gabor@FreeBSD.org>
 #
-# $FreeBSD: ports/Mk/bsd.destdir.mk,v 1.1 2007/08/04 11:37:23 gabor Exp $
+# $FreeBSD: ports/Mk/bsd.destdir.mk,v 1.2 2008/03/11 23:37:32 pav Exp $
 #
 # Please make sure all changes to this file are passed through the maintainer.
 
@@ -208,8 +208,8 @@ do-chroot:
 	${CHROOT} $${_destdir} ${SH} -c "\
 		cd $${PORTSDIR}${.CURDIR:S|^${PORTSDIR}||}; \
 		${SETENV} -i ${DESTDIR_ENV} $${_var_path_list} ${MAKE} ${.TARGETS}" && \
-		${ECHO_MSG} "===>  Chrooted make in ${DESTDIR} succeeded" || \
-		${ECHO_MSG} "===>  Chrooted make in ${DESTDIR} failed"; \
+		{ status=$$?; ${ECHO_MSG} "===>  Chrooted make in ${DESTDIR} succeeded"; } || \
+		{ status=$$?; ${ECHO_MSG} "===>  Chrooted make in ${DESTDIR} failed"; }; \
 		${ECHO_MSG} "===>  Cleaning up..."; \
 	for _entry in $${_mounted_entries_list}; do \
 		${UMOUNT} -f $${_entry} || ${TRUE}; \
@@ -217,6 +217,6 @@ do-chroot:
 	for _entry in $${_created_mountpoints_list}; do \
 		${RMDIR} $${_entry} || ${TRUE}; \
 	done; \
-	exit 0
+	exit $$status
 .endif	# !target(do-chroot)
 .endif	# !defined(_DESTDIRMKINCLUDED)
