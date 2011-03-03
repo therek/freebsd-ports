69fc049fc4fdf2cefb96297f74933080e3e55fdc
ehaupt <ehaupt@FreeBSD.org>
2011-03-03 09:45:05 +0000

    - Add three mirrors to MASTER_SITE_CRITICAL - Remove one out-of-sync mirror

--- Mk/bsd.sites.mk
+++ Mk/bsd.sites.mk
@@ -20,7 +20,7 @@
 #
 # Note: all entries should terminate with a slash.
 #
-# $FreeBSD: ports/Mk/bsd.sites.mk,v 1.527 2011/02/26 16:31:33 az Exp $
+# $FreeBSD: ports/Mk/bsd.sites.mk,v 1.528 2011/02/28 14:09:00 az Exp $
 #
 
 # Where to put distfiles that don't have any other master site
@@ -133,8 +133,10 @@ MASTER_SITE_COMP_SOURCES+=	\
 MASTER_SITE_CRITICAL+=	\
 	http://critical.ch/distfiles/%SUBDIR%/ \
 	http://energy.critical.ch/distfiles/%SUBDIR%/ \
-	http://sauerkraut.critical.ch/distfiles/%SUBDIR%/ \
-	http://snow.critical.ch/distfiles/%SUBDIR%/
+	http://snow.critical.ch/distfiles/%SUBDIR%/ \
+	http://www.bluestop.org/distfiles/%SUBDIR%/ \
+	http://fneu.fr/distfiles/%SUBDIR%/ \
+	ftp://saper.info/pub/FreeBSD/distfiles/%SUBDIR%/
 .endif
 
 .if !defined(IGNORE_MASTER_SITE_CSME)
