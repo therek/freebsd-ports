7868a06100ae3314cf74def1c8e40d67cb60ff2f
wen <wen@FreeBSD.org>
2011-04-01 01:30:52 +0000

    - Fix a typo
    Submitted by:	lichray@gmail.com(via Email)

--- Mk/bsd.emacs.mk
+++ Mk/bsd.emacs.mk
@@ -1,5 +1,5 @@
 #
-#	$FreeBSD: ports/Mk/bsd.emacs.mk,v 1.83 2010/05/14 08:04:38 bsam Exp $
+#	$FreeBSD: ports/Mk/bsd.emacs.mk,v 1.84 2010/09/14 15:26:19 ashish Exp $
 #
 #	bsd.emacs.mk - 19990829 Shigeyuki Fukushima.
 #
@@ -220,7 +220,7 @@ check-makevars::
 #
 
 # find where emacsen is installed
-# look for it in PREEFIX first and fall back to LOCALBASE then
+# look for it in PREFIX first and fall back to LOCALBASE then
 .if exists(/bin/${EMACS_NAME}-${EMACS_VER})
 EMACS_BASE?=			${PREFIX}
 .else
