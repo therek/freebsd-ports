d442b5f3bc91271b60604d81499ac3f70ae6be7e
ohauer <ohauer@FreeBSD.org>
2011-04-24 15:37:53 +0000

    Mk/bsd.apache.mk was rewritten with a new logic and some new parameters   to support future apache versions and the transition to apache22.
      Existing and new USE_APACHE* parameters:
    
      USE_APACHE         set apache and apxs as build and run dependency
      USE_APACHE_BUILD   set apache and apxs as build dependency
      USE_APACHE_RUN     set apache and apxs as run dependency
    
      The following example is representative of all three possible parameters to use:
    
      USE_APACHE= 22     specify exact version
      USE_APACHE= 1.3+   specify [min] version, no [max] version
      USE_APACHE= 13-22  specify [min]-[max] range
      USE_APACHE= -2.2   specify [max] version, no [min] version
    
      Values can be specified with and without "." (dot) between major and minor
      version numbers. For example 22 is equal to 2.2
    
      For documentation and full changelist please take a look into
      Mk/bsd.apache.mk or http://wiki.freebsd.org/bsd.apache.mk
    
      Commit with hat apache@
    
    PR:		ports/156533
    Submitted by:	ohauer

--- Mk/bsd.port.mk
+++ Mk/bsd.port.mk
@@ -1,7 +1,7 @@
 #-*- mode: makefile; tab-width: 4; -*-
 # ex:ts=4
 #
-# $FreeBSD: ports/Mk/bsd.port.mk,v 1.677 2011/03/31 03:37:14 tabthorpe Exp $
+# $FreeBSD: ports/Mk/bsd.port.mk,v 1.678 2011/04/11 08:34:14 bapt Exp $
 #	$NetBSD: $
 #
 #	bsd.port.mk - 940820 Jordan K. Hubbard.
@@ -1536,7 +1536,7 @@ PERL=		${LOCALBASE}/bin/perl
 .include "${PORTSDIR}/Mk/bsd.tcl.mk"
 .endif
 
-.if defined(USE_APACHE)
+.if defined(USE_APACHE) || defined(USE_APACHE_BUILD) || defined(USE_APACHE_RUN)
 .include "${PORTSDIR}/Mk/bsd.apache.mk"
 .endif
 
@@ -2135,7 +2135,7 @@ PLIST_SUB+=		PERL_VERSION=${PERL_VERSION} \
 .include "${PORTSDIR}/Mk/bsd.wx.mk"
 .endif
 
-.if defined(USE_APACHE) || defined(APACHE_COMPAT)
+.if defined(USE_APACHE) || defined(USE_APACHE_BUILD) || defined(USE_APACHE_RUN)
 .include "${PORTSDIR}/Mk/bsd.apache.mk"
 .endif
 
