86cb814e51b7678566d483c268ae2aebec00cc3b
romain <romain@FreeBSD.org>
2011-06-12 19:06:25 +0000

    Hello Mono 2.10!
    Main updates
    ------------
    devel/mono-tools                2.6.2 -> 2.10
    lang/mono                       2.6.7 -> 2.10.2
    lang/mono-basic                 2.6.2 -> 2.10.2
    www/mod_mono                    2.6.3 -> 2.10
    www/xsp                         2.6.5 -> 2.10.2
    x11-toolkits/libgdiplus         2.6.7 -> 2.10
    
    Other updates
    -------------
    deskutils/tomboy                1.4.2 -> 1.6.1
    devel/mono-addins               0.5 -> 0.6.1
    devel/monodevelop               2.4 -> 2.4.2
    graphics/f-spot                 0.8.0 -> 0.8.2
    print/pdfmod                    0.9.0 -> 0.9.1
    x11-toolkits/gnome-sharp20      2.24.1 -> 2.24.2
    
    Ports marked BROKEN
    -------------------
    multimedia/banshee
    multimedia/moonlight
    
    PR:		ports/155948
    Submitted by:	me

--- multimedia/banshee/files/patch-src_Libraries_MusicBrainz_MusicBrainz_LocalDisc.cs
+++ multimedia/banshee/files/patch-src_Libraries_MusicBrainz_MusicBrainz_LocalDisc.cs
@@ -1,18 +1,21 @@
 
-$FreeBSD$
+$FreeBSD: ports/multimedia/banshee/files/patch-src_Libraries_MusicBrainz_MusicBrainz_LocalDisc.cs,v 1.1 2009/11/16 16:29:41 flz Exp $
 
 --- src/Libraries/MusicBrainz/MusicBrainz/LocalDisc.cs.orig
 +++ src/Libraries/MusicBrainz/MusicBrainz/LocalDisc.cs
-@@ -131,7 +131,11 @@
-             try {
+@@ -133,11 +133,11 @@
                  switch (Environment.OSVersion.Platform){
                  case PlatformID.Unix:
--                    return new DiscLinux (device);
-+                        try {
-+                            return new DiscLinux (device);
-+                        } catch {
-+                            return new DiscFreeBSD (device);
-+                        }
+                     // TODO can we actually detect the environment?
+-                    //try {
++                    try {
+                         return new DiscLinux (device);
+-                    //} catch {
+-                    //    return new DiscFreeBSD (device);
+-                    //}
++                    } catch {
++                        return new DiscFreeBSD (device);
++                    }
                  //case PlatformID.Win32NT:
                      //return new DiscWin32NT (device);
                  default:
