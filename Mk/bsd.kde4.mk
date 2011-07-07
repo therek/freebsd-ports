d2e4fe6a646a4873ef888fb8172927332f106bf3
makc <makc@FreeBSD.org>
2011-07-07 16:38:39 +0000

    Update KDE Software Compilation ports to 4.6.5
    Official announcement: http://www.kde.org/announcements/announce-4.6.5.php
    Changelog: http://www.kde.org/announcements/changelogs/changelog4_6_4to4_6_5.php

--- Mk/bsd.kde4.mk
+++ Mk/bsd.kde4.mk
@@ -1,7 +1,7 @@
 #-*- mode: Makefile; tab-width: 4; -*-
 # ex:ts=4
 #
-# $FreeBSD: ports/Mk/bsd.kde4.mk,v 1.118 2011/05/16 19:26:28 makc Exp $
+# $FreeBSD: ports/Mk/bsd.kde4.mk,v 1.119 2011/06/14 09:23:35 avilla Exp $
 #
 
 .if !defined(_POSTMKINCLUDED) && !defined(Kde_Pre_Include)
@@ -54,7 +54,7 @@ Kde_Include_MAINTAINER=	kde@FreeBSD.org
 #		but this could be changed in a future.
 #
 
-KDE4_VERSION=		4.6.4
+KDE4_VERSION=		4.6.5
 KDE4_BRANCH?=		stable
 KDEPIM4_VERSION=	4.4.11.1
 KDEPIM4_BRANCH?=	stable
