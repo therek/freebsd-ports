fb3a6e32ace51468ab3f9ba0364b3f585c133e95
pgj <pgj@FreeBSD.org>
2011-08-13 18:36:10 +0000

    A priority search queue efficiently supports the opperations of both a search tree and a priority queue. A 'Binding' is a product of a key and a priority.  Bindings can be inserted, deleted, modified and queried in logarithmic time, and the binding with the least priority can be retrieved in constant time.  A queue can be built from a list of bindings, sorted by keys, in linear time.
    WWW: http://hackage.haskell.org/package/PSQueue
    
    Obtained from:	FreeBSD Haskell

--- lang/ghc/bsd.hackage.mk
+++ lang/ghc/bsd.hackage.mk
@@ -1,5 +1,5 @@
 #
-# $FreeBSD: ports/lang/ghc/bsd.hackage.mk,v 1.36 2011/08/01 13:43:49 pgj Exp $
+# $FreeBSD: ports/lang/ghc/bsd.hackage.mk,v 1.37 2011/08/13 18:08:39 pgj Exp $
 #
 # bsd.hackage.mk -- List of Haskell Cabal ports.
 #
@@ -124,6 +124,7 @@ polyparse_port=			textproc/hs-polyparse
 porte_port=			ports-mgmt/hs-porte	# executable
 primitive_port=			devel/hs-primitive
 probability_port=		math/hs-probability
+PSQueue_port=			devel/hs-PSQueue
 QuickCheck_port=		devel/hs-QuickCheck
 reactive_port=			devel/hs-reactive	# BROKEN
 readline_port=			devel/hs-readline
