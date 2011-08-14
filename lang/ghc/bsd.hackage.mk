6b0f763e3d2ffe239841986b64bb28085331e02d
pgj <pgj@FreeBSD.org>
2011-08-14 17:52:20 +0000

    Efficient hashing-based container types. The containers have been optimized for performance critical use, both in terms of large data quantities and high speed.
    The declared cost of each operation is either worst-case or amortized,
    but remains valid even if structures are shared.
    
    WWW:	http://github.com/tibbe/unordered-containers/
    
    Obtained from:	FreeBSD Haskell

--- lang/ghc/bsd.hackage.mk
+++ lang/ghc/bsd.hackage.mk
@@ -1,5 +1,5 @@
 #
-# $FreeBSD: ports/lang/ghc/bsd.hackage.mk,v 1.67 2011/08/14 17:46:17 pgj Exp $
+# $FreeBSD: ports/lang/ghc/bsd.hackage.mk,v 1.68 2011/08/14 17:49:44 pgj Exp $
 #
 # bsd.hackage.mk -- List of Haskell Cabal ports.
 #
@@ -188,6 +188,7 @@ unamb_port=			devel/hs-unamb
 uniplate_port=			devel/hs-uniplate
 unix-compat_port=		devel/hs-unix-compat
 unlambda_port=			lang/hs-unlambda	# executable
+unordered-containers_port=	devel/hs-unordered-containers
 url_port=			www/hs-url
 utf8-string_port=		devel/hs-utf8-string
 utility-ht_port=		devel/hs-utility-ht
