6848d5979348cc77b139879944264c445049e512
pawel <pawel@FreeBSD.org>
2011-09-17 14:55:56 +0000

    - Update to version 2.2p2 - Assign mainatinership to submitter - Update WWW
    PR:		ports/159735
    Submitted by:	Edward Sanford Sutton, III <mirror176@cox.net>
    Approved by:	miwi, wen (mentors implicit)

--- comms/rxtx/files/patch-src-ParallelImp.c
+++ comms/rxtx/files/patch-src-ParallelImp.c
@@ -1,4 +1,4 @@
-$FreeBSD$
+$FreeBSD: ports/comms/rxtx/files/patch-ParallelImp.c,v 1.1 2004/10/20 11:17:36 hq Exp $
 
 --- src/ParallelImp.c.orig	Fri Apr  9 23:31:09 2004
 +++ src/ParallelImp.c	Fri Apr  9 23:35:01 2004
