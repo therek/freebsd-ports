f22cf2f959302b1cfce1c36c3bdfca0fca8f47ce
avilla <avilla@FreeBSD.org>
2012-01-25 10:21:01 +0000

    The KDE/FreeBSD team is pleased to announce KDE SC 4.7.4, which concludes the 4.7 series.
    The official release notes can be found at:
    http://kde.org/announcements/announce-4.7.4.php
    
    PR:		162216
    PR:		163662

--- Mk/bsd.kde4.mk
+++ Mk/bsd.kde4.mk
@@ -1,7 +1,7 @@
 #-*- mode: Makefile; tab-width: 4; -*-
 # ex:ts=4
 #
-# $FreeBSD: ports/Mk/bsd.kde4.mk,v 1.123 2011/10/29 00:49:39 avilla Exp $
+# $FreeBSD: ports/Mk/bsd.kde4.mk,v 1.124 2011/11/14 02:59:22 rakuco Exp $
 #
 
 .if !defined(_POSTMKINCLUDED) && !defined(Kde_Pre_Include)
@@ -54,7 +54,7 @@ Kde_Include_MAINTAINER=	kde@FreeBSD.org
 #		but this could be changed in a future.
 #
 
-KDE4_VERSION=		4.7.3
+KDE4_VERSION=		4.7.4
 KDE4_BRANCH?=		stable
 KDEPIM4_VERSION=	4.4.11.1
 KDEPIM4_BRANCH?=	stable
