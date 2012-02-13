3e25b20708b31d2566275857961b238073aa61d9
pgj <pgj@FreeBSD.org>
2012-02-13 02:17:35 +0000

    Haskell implementation of Mustache templates [1].
    [1] http://mustache.github.com/
    
    WWW:	http://github.com/lymar/hastache
    
    Obtained from:	FreeBSD Haskell

--- lang/ghc/bsd.hackage.mk
+++ lang/ghc/bsd.hackage.mk
@@ -1,5 +1,5 @@
 #
-# $FreeBSD: ports/lang/ghc/bsd.hackage.mk,v 1.79 2012/02/13 02:13:47 pgj Exp $
+# $FreeBSD: ports/lang/ghc/bsd.hackage.mk,v 1.80 2012/02/13 02:15:56 pgj Exp $
 #
 # bsd.hackage.mk -- List of Haskell Cabal ports.
 #
@@ -95,6 +95,7 @@ hashtables_port=		devel/hs-hashtables
 haskeline_port=			devel/hs-haskeline
 haskell-src_port=		devel/hs-haskell-src
 haskell-src-exts_port=		devel/hs-haskell-src-exts
+hastache_port=			devel/hs-hastache
 HaXml_port=			textproc/hs-HaXml	# executable
 heist_port=			www/hs-heist
 HGL_port=			graphics/hs-HGL
