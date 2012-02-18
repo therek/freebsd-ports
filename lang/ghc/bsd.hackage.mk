3ed5c6f47b06fe8a590173439877ed82a70ee210
pgj <pgj@FreeBSD.org>
2012-02-18 17:46:39 +0000

    Form handling support for Yesod Web Framework.
    WWW:	http://www.yesodweb.com/
    
    Obtained from:	FreeBSD Haskell

--- lang/ghc/bsd.hackage.mk
+++ lang/ghc/bsd.hackage.mk
@@ -1,5 +1,5 @@
 #
-# $FreeBSD: ports/lang/ghc/bsd.hackage.mk,v 1.155 2012/02/18 17:42:42 pgj Exp $
+# $FreeBSD: ports/lang/ghc/bsd.hackage.mk,v 1.156 2012/02/18 17:44:40 pgj Exp $
 #
 # bsd.hackage.mk -- List of Haskell Cabal ports.
 #
@@ -292,6 +292,7 @@ xmonad-contrib_port=		x11-wm/hs-xmonad-contrib
 xss-sanitize_port=		www/hs-xss-sanitize
 yaml_port=			textproc/hs-yaml
 yesod-core_port=		www/hs-yesod-core
+yesod-form_port=		www/hs-yesod-form
 yesod-persistent_port=		www/hs-yesod-persistent
 zip-archive_port=		archivers/hs-zip-archive
 zlib_port=			archivers/hs-zlib
