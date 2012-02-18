b5eec9afe28b42b65e342c9404b8b9d6130f109a
pgj <pgj@FreeBSD.org>
2012-02-18 17:53:13 +0000

    A RESTful web framework with strong compile-time guarantees of correctness. It also affords space efficient code, highly concurrent loads, and portability to many deployment backends (via the wai package), from CGI to stand-alone serving. Yesod also focuses on developer productivity. Yesod integrates well with tools for all your basic web development (wai, persistent, and shakespeare/hamlet).
    WWW:	http://www.yesodweb.com/
    
    Obtained from:	FreeBSD Haskell

--- lang/ghc/bsd.hackage.mk
+++ lang/ghc/bsd.hackage.mk
@@ -1,5 +1,5 @@
 #
-# $FreeBSD: ports/lang/ghc/bsd.hackage.mk,v 1.158 2012/02/18 17:48:55 pgj Exp $
+# $FreeBSD: ports/lang/ghc/bsd.hackage.mk,v 1.159 2012/02/18 17:51:06 pgj Exp $
 #
 # bsd.hackage.mk -- List of Haskell Cabal ports.
 #
@@ -291,6 +291,7 @@ xmonad_port=			x11-wm/hs-xmonad	# executable
 xmonad-contrib_port=		x11-wm/hs-xmonad-contrib
 xss-sanitize_port=		www/hs-xss-sanitize
 yaml_port=			textproc/hs-yaml
+yesod_port=			www/hs-yesod
 yesod-auth_port=		www/hs-yesod-auth
 yesod-core_port=		www/hs-yesod-core
 yesod-form_port=		www/hs-yesod-form
