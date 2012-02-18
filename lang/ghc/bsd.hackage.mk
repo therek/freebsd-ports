d80d762b34b801795518ea5fc816949d3e69fff0
pgj <pgj@FreeBSD.org>
2012-02-18 17:18:34 +0000

    Components of paths.
    WWW:	http://github.com/snoyberg/path-pieces
    
    Obtained from:	FreeBSD Haskell

--- lang/ghc/bsd.hackage.mk
+++ lang/ghc/bsd.hackage.mk
@@ -1,5 +1,5 @@
 #
-# $FreeBSD: ports/lang/ghc/bsd.hackage.mk,v 1.143 2012/02/18 17:13:17 pgj Exp $
+# $FreeBSD: ports/lang/ghc/bsd.hackage.mk,v 1.144 2012/02/18 17:15:46 pgj Exp $
 #
 # bsd.hackage.mk -- List of Haskell Cabal ports.
 #
@@ -188,6 +188,7 @@ pandoc-types_port=		textproc/hs-pandoc-types
 pango_port=			x11-toolkits/hs-pango
 parallel_port=			devel/hs-parallel
 parsec_port=			textproc/hs-parsec
+path-pieces_port=		www/hs-path-pieces
 pcap_port=			net/hs-pcap
 pcre-light_port=		devel/hs-pcre-light	# lib_depends
 polyparse_port=			textproc/hs-polyparse
