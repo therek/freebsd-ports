de167ee6c2937b757c310782fe5ddc4f53cd3a1e
pgj <pgj@FreeBSD.org>
2012-02-18 16:24:24 +0000

    A set of extra definitions, default values and helpers for the tls package.
    WWW:	http://github.com/vincenthz/hs-tls-extra
    
    Obtained from:	FreeBSD Haskell

--- lang/ghc/bsd.hackage.mk
+++ lang/ghc/bsd.hackage.mk
@@ -1,5 +1,5 @@
 #
-# $FreeBSD: ports/lang/ghc/bsd.hackage.mk,v 1.125 2012/02/18 16:19:56 pgj Exp $
+# $FreeBSD: ports/lang/ghc/bsd.hackage.mk,v 1.126 2012/02/18 16:23:00 pgj Exp $
 #
 # bsd.hackage.mk -- List of Haskell Cabal ports.
 #
@@ -231,6 +231,7 @@ testpack_port=			devel/hs-testpack
 texmath_port=			textproc/hs-texmath	# executable
 text_port=			devel/hs-text
 tls_port=			security/hs-tls
+tls-extra_port=			security/hs-tls-extra
 transformers_port=		devel/hs-transformers
 transformers-base_port=		devel/hs-transformers-base
 TypeCompose_port=		devel/hs-TypeCompose
